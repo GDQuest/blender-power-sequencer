bee5f9a822a406d80206d317d2a6c3f3cbe2ea55
Nathan <nathan@gdquest.com>
2017-07-21 12:31:44 +0200

    fix variable referenced before assignment

--- video_editing_mouse.py
+++ video_editing_mouse.py
@@ -123,8 +123,7 @@ class MouseCut(bpy.types.Operator):
                 selection = bpy.context.selected_sequences
                 sorted_sequences = sorted(selection,
                                    key=attrgetter('frame_final_start'))
-                if len(sorted_sequences) > 1:
-                    first_seq = sorted_sequences[0].frame_final_start
+                first_seq = sorted_sequences[0]
                 
                 frame = first_seq.frame_final_start - self.cursor_offset \
                     if abs(frame - first_seq.frame_final_start) < first_seq.frame_final_duration / 2 \
