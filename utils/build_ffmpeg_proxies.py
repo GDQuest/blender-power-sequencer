4efd4f150b59469e22dbb9a38021f9f636eecba7
Nathan <nathan@gdquest.com>
2017-08-15 13:09:06 +0200

    add 25% video proxy generation script

--- utils/build_ffmpeg_proxies.py
+++ utils/build_ffmpeg_proxies.py
@@ -5,30 +5,43 @@ from subprocess import call
 # Don't re-create proxies if they already exist
 # Upon generating a proxy, save the file path and update the time stamp
 
+VIDEO_TYPES = ['.mkv']
+IMAGE_TYPES = ['']
+
+files = []
+
 # Gather files to generate proxies for and create BL_proxy dirs
-video_files, image_files = [], []
 for dirpath, _, filenames in os.walk('.'):
     for f in filenames:
         file_name, file_extension = os.path.splitext(f)
-        if file_extension in IMAGE_TYPES:
-            image_files.append(os.path.join(dirpath, f))
-        elif file_extension in VIDEO_TYPES:
-            video_files.append(os.path.join(dirpath, f))
+        abs_dir = os.path.abspath(dirpath)
+        if file_extension in IMAGE_TYPES or file_extension in VIDEO_TYPES:
+            files.append(os.path.join(abs_dir, f))
 
 # Make proxy dirs before generating proxies
-all_files = video_files.extend(image_files)
-for f in all_files:
-    dirpath, filename = os.path.splitext(f)
+for filepath in files:
+    dirpath, filename = os.path.split(filepath)
+    _, extension = os.path.splitext(filename)
+
     bl_proxy_folder = os.path.join(dirpath, 'BL_proxy')
-    if not os.path.isdir(bl_proxy_folder):
+    if not os.path.exists(bl_proxy_folder):
         os.mkdir(bl_proxy_folder)
 
+    proxy_folder = os.path.join(bl_proxy_folder, filename)
+    print(proxy_folder)
+    if not os.path.exists(proxy_folder):
+        os.mkdir(proxy_folder)
 
-for f in video_files:
-    ffmpeg_command = "ffmpeg -i {!s} -strict experimental -hide_banner -f matroska -map_chapters -1 -map 0:0 -sn -an -c:v:0 mpeg2video -b:v:0 1800k -mbd:v:0 rd -mbcmp:v:0 rd -cmp:v:0 rd -precmp:v:0 rd -subcmp:v:0 rd -trellis:v:0 1 -filter:v:0 \"scale=480:270\" -y {!s}".format("\"" + f + "\"", "\"" + "BL_proxy/" + f + "/proxy_25.avi" + "\"")
+    # ffmpeg build
+    # TODO: add support for different proxy sizes
+    # TODO: add support for img files
+    export_path = os.path.join(proxy_folder, 'proxy_25.avi')
+    ffmpeg_command = ''
+    if extension in VIDEO_TYPES:
+        ffmpeg_command = "ffmpeg -i {!s} -strict experimental -hide_banner -f matroska -map_chapters -1 -map 0:0 -sn -an -c:v:0 mpeg2video -b:v:0 1800k -mbd:v:0 rd -mbcmp:v:0 rd -cmp:v:0 rd -precmp:v:0 rd -subcmp:v:0 rd -trellis:v:0 1 -filter:v:0 \"scale=480:270\" -y {!s}".format(filepath, export_path)
+    # elif extension in IMAGE_TYPES:
+    #     ffmpeg_command = "ffmpeg -i {!s} -vf \"scale=480:270\" {!s}".format("\"" + f + "\"", "\"" + "BL_proxy/" + f + "/proxy_25.png" + "\"")
+
     call(ffmpeg_command, shell=True)
 
-# TODO: Check image proxy expected filename - we likely have to build as png and rename it
-for f in image_files:
-    ffmpeg_command = "ffmpeg -i {!s} -vf \"scale=480:270\" {!s}".format("\"" + f + "\"", "\"" + "BL_proxy/" + f + "/proxy_25.png" + "\"")
-    call(ffmpeg_command, shell=True)
+
