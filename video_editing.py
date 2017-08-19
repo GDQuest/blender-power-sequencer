f4c54b6186f85a051a1e6ab4c37d8ef49557a0f9
Nathan <nathan@gdquest.com>
2017-08-19 12:47:19 +0200

    Add operator to deselect handles and move the sequence

--- video_editing.py
+++ video_editing.py
@@ -549,6 +549,30 @@ class GrabSequenceHandles(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class DeselectHandlesAndGrab(bpy.types.Operator):
+    """
+    Deselect the handles of all selected strips and call the
+    Sequence Slide operator
+    """
+    bl_idname = 'power_sequencer.deselect_handles_seq_slide'
+    bl_label = 'PS.Deselect handles and grab'
+    bl_description = ''
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        for s in bpy.context.selected_sequences:
+            s.select_left_handle = False
+            s.select_right_handle = False
+            s.select = True
+
+        bpy.ops.transform.seq_slide('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+
 class PreviewLastCut(bpy.types.Operator):
     """
     Finds the closest cut to the time cursor and
