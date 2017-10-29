9c7a3a4bf3df25a91db4fed058999b51e951d711
Nathan <nathan@gdquest.com>
2017-10-29 11:45:48 +0100

    Add replacement for cut and paste selection in sequencer

--- simple_operations.py
+++ simple_operations.py
@@ -1,6 +1,6 @@
 """Simple operations like save, delete, open the project directory..."""
 import bpy
-from bpy.props import EnumProperty
+from bpy.props import EnumProperty, BoolProperty
 from operator import attrgetter
 from .functions.sequences import get_frame_range, set_preview_range, \
     slice_selection
@@ -79,6 +79,50 @@ class DeleteDirect(bpy.types.Operator):
         selection_length = len(selection)
         report_message = 'Deleted ' + str(selection_length) + ' sequence'
         report_message += 's' if selection_length > 1 else ''
+        self.report({'INFO'}, report_message)
+        return {"FINISHED"}
+
+
+class CopySelectedSequences(bpy.types.Operator):
+    """
+    Copies the selected sequences without frame offset and optionally deletes the selection to give a cut to clipboard effect
+    """
+    bl_idname = "power_sequencer.copy_selection"
+    bl_label = "PS.Copy or cut sequences"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    delete_selection = BoolProperty(
+        name="Delete selection",
+        description="Delete selected strips: acts like cut and paste",
+        default=False)
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        selection = bpy.context.selected_sequences
+        cursor_start_frame = bpy.context.scene.frame_current
+        sequencer = bpy.ops.sequencer
+
+        # Deactivate audio playback
+        scene = bpy.context.scene
+        initial_audio_setting = scene.use_audio_scrub
+        scene.use_audio_scrub = False
+
+        first_sequence = min(selection, key=attrgetter('frame_final_start'))
+        bpy.context.scene.frame_current = first_sequence.frame_final_start
+        sequencer.copy()
+        bpy.context.scene.frame_current = cursor_start_frame
+
+        scene.use_audio_scrub = initial_audio_setting
+
+        if self.delete_selection:
+            sequencer.delete()
+
+        plural_string = 's' if len(selection) != 1 else ''
+        action_verb = 'Cut' if self.delete_selection else 'Copied'
+        report_message = '{!s} {!s} sequence{!s} to the clipboard.'.format(action_verb, str(len(selection)), plural_string)
         self.report({'INFO'}, report_message)
         return {"FINISHED"}
 
