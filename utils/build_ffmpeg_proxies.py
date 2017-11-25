7169bc5e7141f955ee484f57ce2beef795f317af
Nathan <nathan@gdquest.com>
2017-11-25 15:26:03 +0100

    Fix error if path contains spaces
    On windows if the path contained a space the command would break, even when escaping whitespace
    Now the script uses subprocess.Popen and an argument list to avoid the issue

--- utils/build_ffmpeg_proxies.py
+++ utils/build_ffmpeg_proxies.py
@@ -30,6 +30,7 @@ Extra ideas:
 import os
 import subprocess
 import sys
+import logging
 
 
 def parse_cmd_args():
@@ -46,13 +47,14 @@ def parse_cmd_args():
 project_folder = parse_cmd_args()
 if not project_folder:
     project_folder = '.'
+    logging.warning('The folder passed as an argument isn\'t valid. Using the current folder instead')
 
 
 VIDEO_EXT = ['.mp4', '.mkv']
 IMG_EXT = ['.png', '.jpg', '.jpeg']
 
-FFMPEG_VIDEO_COMMAND = "ffmpeg -i {!s} -strict experimental -hide_banner -f matroska -map_chapters -1 -map 0:0 -sn -an -c:v:0 mpeg2video -b:v:0 1800k -mbd:v:0 rd -mbcmp:v:0 rd -cmp:v:0 rd -precmp:v:0 rd -subcmp:v:0 rd -trellis:v:0 1 -filter:v:0 \"scale=iw*0.25:ih*0.25\" -y {!s}"
-FFMPEG_IMAGE_COMMAND = "ffmpeg -i {!s} -vf scale=iw*0.25:ih*0.25 {!s}"
+FFMPEG_VIDEO_COMMAND = ["ffmpeg", "-i", "", "-hide_banner", "-f", "matroska", "-sn", "-an", "-c:v", "mpeg2video", "-b:v", "1800k", "-filter:v", "scale=iw*0.25:ih*0.25", "-y", ""]
+FFMPEG_IMAGE_COMMAND = ["ffmpeg", "-i", "", "-hide_banner", "-vf", "scale=iw*0.25:ih*0.25", ""]
 
 ffmpeg_commands = []
 img_proxy_folders = []
@@ -70,7 +72,11 @@ for dirpath, dirnames, filenames in os.walk(project_folder):
             video_proxy_subdir = os.path.join(dirpath, 'BL_proxy/{!s}/'.format(f))
             if not os.path.isdir(video_proxy_subdir):
                 os.makedirs(video_proxy_subdir)
-            ffmpeg_commands.append(FFMPEG_IMAGE_COMMAND.format(file_path, os.path.join(video_proxy_subdir, 'proxy_25.avi')))
+
+            new_command = FFMPEG_VIDEO_COMMAND
+            new_command[2] = file_path
+            new_command[-1] = os.path.join(video_proxy_subdir, 'proxy_25.avi')
+            ffmpeg_commands.append(new_command)
 
         elif extension in IMG_EXT:
             img_proxy_folder = os.path.join(dirpath, 'BL_proxy/images/25/')
@@ -79,14 +85,17 @@ for dirpath, dirnames, filenames in os.walk(project_folder):
                     os.makedirs(img_proxy_folder)
                 img_proxy_folders.append(img_proxy_folder)
                 found_image = True
+            new_command = FFMPEG_IMAGE_COMMAND
+            new_command[2] = file_path
+            new_command[-1] = os.path.join(img_proxy_folder, f)
             # Keep the original img extension so ffmpeg retains transparency on PNG, append _proxy.jpg suffix in a second pass
-            ffmpeg_commands.append(FFMPEG_IMAGE_COMMAND.format(file_path, os.path.join(img_proxy_folder, f)))
+            ffmpeg_commands.append(new_command)
 
 # print(ffmpeg_commands)
 # print(img_proxy_folders)
 
 for command in ffmpeg_commands:
-    subprocess.call(command, shell=True)
+    subprocess.Popen(command)
 
 for folder in img_proxy_folders:
     # print(folder)
