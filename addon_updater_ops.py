9ad20a9942f693d8d342f8c884a32ae2c8fba1fd
TheDuckCow <theduckcow@live.com>
2018-01-02 00:32:05 -0500

    updated min version for updater to 1.0.0
    As per conversation: https://github.com/GDquest/Blender-power-sequencer/issues/12#issuecomment-354705166

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1120,7 +1120,7 @@ def register(bl_info):
 	# Set the min and max versions allowed to install.
 	# Optional, default None
 	# min install (>=) will install this and higher
-	updater.version_min_update = (0,1,0) 
+	updater.version_min_update = (1,0,0) 
 	# updater.version_min_update = None  # if not wanting to define a min
 	
 	# max install (<) will install strictly anything lower
