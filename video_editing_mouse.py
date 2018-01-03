927c5e04ecb6eae8bbc71ef2a75562cd09d1a5c6
Nathan <nathan@gdquest.com>
2018-01-03 21:19:41 +0100

    refactor grab_closest_handle_or_cut to prepare for detection code rewrite

--- video_editing_mouse.py
+++ video_editing_mouse.py
@@ -371,41 +371,9 @@ def find_closest_surrounding_cuts(frame=0):
     return start_cut_frame, end_cut_frame
 
 
-def find_cut_and_handles_closest_to_mouse(mouse_x, mouse_y):
-    """
-    takes the mouse's coordinates in the sequencer area and returns the two strips
-    who share the cut closest to the mouse, or the strip with the closest handle.
-    Use it to find the handle(s) to select with the grab on the fly operator
-    """
-    view2d = bpy.context.region.view2d
 
-    closest_cut = (None, None)
-    distance_to_closest_cut = 1000000.0
-
-    for s in bpy.context.sequences:
-        channel_offset = s.channel + 0.5
-        start_x, start_y = view2d.view_to_region(s.frame_final_start, channel_offset)
-        end_x, end_y = view2d.view_to_region(s.frame_final_start, channel_offset)
-
-        distance_to_start = calculate_distance(start_x, start_y, mouse_x, mouse_y)
-        distance_to_end = calculate_distance(end_x, end_y, mouse_x, mouse_y)
-
-        if distance_to_start < distance_to_closest_cut:
-            closest_cut = (start_x, start_y)
-            distance_to_closest_cut = distance_to_start
-        if distance_to_end < distance_to_closest_cut:
-            closest_cut = (end_x, end_y)
-            distance_to_closest_cut = distance_to_end
-
-    closest_cut_local_coords = view2d.region_to_view(closest_cut[0], closest_cut[1])
-    frame, channel = round(closest_cut_local_coords[0]), floor(closest_cut_local_coords[1])
-    return frame, channel
-
-
-def calculate_distance(x1, y1, x2, y2):
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
-
+# TODO: rewrite self.find_cut_and_handles_closest_to_mouse
+# use find_snap_candidate? at least a similar approach
 class GrabClosestHandleOrCut(bpy.types.Operator):
     """
     Selects and grabs the strip handle or cut closest to the mouse cursor.
@@ -427,13 +395,15 @@ class GrabClosestHandleOrCut(bpy.types.Operator):
         view2d = bpy.context.region.view2d
 
         mouse_x, mouse_y = event.mouse_region_x, event.mouse_region_y
-        frame, channel = find_cut_and_handles_closest_to_mouse(mouse_x, mouse_y)
+        frame, channel = self.find_cut_and_handles_closest_to_mouse(mouse_x, mouse_y)
+
 
         matching_sequences = []
         for s in bpy.context.sequences:
-            if s.channel == channel:
-                if abs(s.frame_final_start - frame) <= 1 or abs(s.frame_final_end - frame) <= 1:
-                    matching_sequences.append(s)
+            if not s.channel == channel:
+                continue
+            if abs(s.frame_final_start - frame) <= 1 or abs(s.frame_final_end - frame) <= 1:
+                matching_sequences.append(s)
         matching_count = len(matching_sequences)
 
         sequencer.select_all(action='DESELECT')
@@ -444,6 +414,7 @@ class GrabClosestHandleOrCut(bpy.types.Operator):
                 sequence.select_left_handle = True
             elif abs(sequence.frame_final_end - frame) <= 1:
                 sequence.select_right_handle = True
+
         elif matching_count == 2:
             cut_select_range = 40
             sorted_sequences = sorted(matching_sequences, key=attrgetter('frame_final_start'))
@@ -458,13 +429,48 @@ class GrabClosestHandleOrCut(bpy.types.Operator):
                     second_sequence.select = True
                     second_sequence.select_left_handle = True
             else:
-
                 first_sequence.select = True
                 first_sequence.select_right_handle = True
                 second_sequence.select = True
                 second_sequence.select_left_handle = True
-        bpy.ops.transform.seq_slide('INVOKE_DEFAULT')
-        return {'FINISHED'}
+
+        return bpy.ops.transform.seq_slide('INVOKE_DEFAULT')
+
+
+    def find_cut_and_handles_closest_to_mouse(self, mouse_x, mouse_y):
+        """
+        takes the mouse's coordinates in the sequencer area and returns the two strips
+        who share the cut closest to the mouse, or the strip with the closest handle.
+        Use it to find the handle(s) to select with the grab on the fly operator
+        """
+        view2d = bpy.context.region.view2d
+
+        closest_cut = (None, None)
+        distance_to_closest_cut = 1000000.0
+
+        for s in bpy.context.sequences:
+            channel_offset = s.channel + 0.5
+            start_x, start_y = view2d.view_to_region(s.frame_final_start, channel_offset)
+            end_x, end_y = view2d.view_to_region(s.frame_final_start, channel_offset)
+
+            distance_to_start = self.calculate_distance(start_x, start_y, mouse_x, mouse_y)
+            distance_to_end = self.calculate_distance(end_x, end_y, mouse_x, mouse_y)
+
+            if distance_to_start < distance_to_closest_cut:
+                closest_cut = (start_x, start_y)
+                distance_to_closest_cut = distance_to_start
+            if distance_to_end < distance_to_closest_cut:
+                closest_cut = (end_x, end_y)
+                distance_to_closest_cut = distance_to_end
+
+        closest_cut_local_coords = view2d.region_to_view(closest_cut[0], closest_cut[1])
+        frame, channel = round(closest_cut_local_coords[0]), floor(closest_cut_local_coords[1])
+        return frame, channel
+
+
+    def calculate_distance(self, x1, y1, x2, y2):
+        return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
 
 
 class TrimToSurroundingCuts(bpy.types.Operator):
