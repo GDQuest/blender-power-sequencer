245bcc8cc4457778abc1cbf2e725a2293afd1b83
Nathan Lovato <nathan@gdquest.com>
2018-07-18 08:37:56 +0900

    Fix two bugs with remove_gaps
    closes #161, 163

--- operators/remove_gaps.py
+++ operators/remove_gaps.py
@@ -15,7 +15,7 @@ class RemoveGaps(bpy.types.Operator):
         name="Ignore Locked Strips",
         description="Remove gaps without moving locked strips",
         default=True)
-    remove_all = bpy.props.BoolProperty(
+    all = bpy.props.BoolProperty(
         name="Remove All",
         description="Remove all gaps starting from the time cursor",
         default=False)
@@ -39,10 +39,15 @@ class RemoveGaps(bpy.types.Operator):
         else:
             first_gap_frame = -1
             last_sequence = sorted_sequences[0]
+            last_biggest_frame_end = last_sequence.frame_final_end
             for s in sorted_sequences:
-                if last_sequence.frame_final_end < s.frame_final_start:
+                if last_biggest_frame_end >= s.frame_final_start:
+                    last_sequence = s
+                    continue
+                elif last_sequence.frame_final_end < s.frame_final_start:
                     first_gap_frame = last_sequence.frame_final_end
                     break
+                last_biggest_frame_end = max(last_biggest_frame_end, s.frame_final_end)
                 last_sequence = s
 
         if first_gap_frame == -1:
@@ -58,9 +63,10 @@ class RemoveGaps(bpy.types.Operator):
                                    if s.frame_final_start >= first_gap_frame]
 
         sequence_blocks = slice_selection(sequences_after_gap)
-        # print("%s sequence blocks found" % len(sequence_blocks))
-        # for block in sequence_blocks:
-        #     print("Block start {!s}, end {!s}, len {!s}".format(block[0].frame_final_start, block[-1].frame_final_end, len(block)))
+        print("Gap frame: %s" % first_gap_frame)
+        print("%s sequence blocks found" % len(sequence_blocks))
+        for block in sequence_blocks:
+            print("Block start {!s}, end {!s}, len {!s}".format(block[0].frame_final_start, block[-1].frame_final_end, len(block)))
 
 
         # FIXME: can't read strips with inputs
@@ -69,16 +75,16 @@ class RemoveGaps(bpy.types.Operator):
         for sequence_block in sequence_blocks:
             sorted_block = sorted(sequence_block, key=attrgetter('frame_final_start', 'frame_final_end'))
             gap_size = sorted_block[0].frame_final_start - current_gap_start_frame
-            print("Gap size: %s" % gap_size)
+            # print("Gap size: %s" % gap_size)
             if gap_size < 1:
                 continue
 
             for s in sorted_block:
-                if s in SequenceTypes.EFFECT:
-                    continue
-                s.frame_start -= gap_size
-
-            if not self.remove_all:
+                try:
+                    s.frame_start -= gap_size
+                except AttributeError:
+                    pass
+            if not self.all:
                 break
             current_gap_start_frame = sorted_block[-1].frame_final_end
         return {'FINISHED'}
