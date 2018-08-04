da47d2ffd5feeb4fcf3da9edd48516f30b6a580f
Nathan Lovato <nathan@gdquest.com>
2018-08-04 09:40:11 +0900

    Clean print statements
    closes #167

--- operators/utils/find_closest_strip.py
+++ operators/utils/find_closest_strip.py
@@ -22,7 +22,7 @@ def find_closest_strip(sequences, mouse_x, mouse_y):
 
         distance_smallest = min(distance_to_left_handle, distance_to_right_handle)
         if distance_smallest < distance_to_closest_strip:
-            print("Smallest distance: {}, {!s}".format(s.name, distance_smallest))
+            # print("Smallest distance: {}, {!s}".format(s.name, distance_smallest))
             closest_strip = s
             distance_to_closest_strip = distance_smallest
     return closest_strip
