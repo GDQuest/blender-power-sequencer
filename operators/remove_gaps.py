5a5bea1188d13fbd37abe29cf3cb230a9e8a05d1
Nathan Lovato <nathan@gdquest.com>
2018-08-29 13:58:52 +0900

    Refactor and fix first frame detection
    Simplify and optimize the code for greater performances
    
    Fix #212
    Fix #170
    Fix #181

--- operators/remove_gaps.py
+++ operators/remove_gaps.py
@@ -15,7 +15,7 @@ class RemoveGaps(bpy.types.Operator):
         name="Ignore Locked Strips",
         description="Remove gaps without moving locked strips",
         default=True)
-    all = bpy.props.BoolProperty(
+    remove_all = bpy.props.BoolProperty(
         name="Remove All",
         description="Remove all gaps starting from the time cursor",
         default=False)
@@ -24,67 +24,53 @@ class RemoveGaps(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context):
-        return len(context.scene.sequence_editor.sequences) > 0
+        return len(context.sequences) > 0
 
     def execute(self, context):
-        if self.frame_start_override > -1:
-            frame_start = self.frame_start_override
-        else:
-            frame_start = context.scene.frame_current
+        frame_start = self.frame_start_override if self.frame_start_override > -1 else context.scene.frame_current
 
-        sorted_sequences = sorted(context.scene.sequence_editor.sequences, key=attrgetter('frame_final_start'))
-        first_sequence_start = sorted_sequences[0].frame_final_start
-        if first_sequence_start > frame_start:
-            first_gap_frame = frame_start
-        else:
-            first_gap_frame = -1
-            last_sequence = sorted_sequences[0]
-            last_biggest_frame_end = last_sequence.frame_final_end
-            for s in sorted_sequences:
-                if last_biggest_frame_end >= s.frame_final_start:
-                    last_sequence = s
-                    continue
-                elif last_sequence.frame_final_end < s.frame_final_start:
-                    first_gap_frame = last_sequence.frame_final_end
-                    break
-                last_biggest_frame_end = max(last_biggest_frame_end, s.frame_final_end)
-                last_sequence = s
+        sequences_to_process = (s for s in context.sequences if s.frame_final_start >= frame_start or s.frame_final_end > frame_start)
+        sequence_blocks = slice_selection(sequences_to_process)
+        # DEBUG prints
+        # print("%s sequence blocks found" % len(sequence_blocks))
+        # for block in sequence_blocks:
+        #     print("Block start {!s}, end {!s}, len {!s}".format(block[0].frame_final_start, block[-1].frame_final_end, len(block)))
 
-        if first_gap_frame == -1:
+        first_gap_frame = self.find_first_gap_frame(sequence_blocks[0], frame_start)
+        if first_gap_frame == None:
             return {'FINISHED'}
+        print(first_gap_frame)
 
-        sequences_after_gap = []
-        if self.ignore_locked:
-            sequences_after_gap = [s for s in sorted_sequences
-                                   if s.frame_final_start >= first_gap_frame and
-                                   not s.lock]
-        else:
-            sequences_after_gap = [s for s in sorted_sequences
-                                   if s.frame_final_start >= first_gap_frame]
+        self.remove_gaps(sequence_blocks[1:], first_gap_frame, self.remove_all)
+        return {'FINISHED'}
 
-        sequence_blocks = slice_selection(sequences_after_gap)
-        print("Gap frame: %s" % first_gap_frame)
-        print("%s sequence blocks found" % len(sequence_blocks))
+    def find_first_gap_frame(self, sorted_sequences, frame_start):
+        sequences_frame_start = min(sorted_sequences, key=attrgetter('frame_final_start')).frame_final_start
+        sequences_frame_end = max(sorted_sequences, key=attrgetter('frame_final_end')).frame_final_end
+        return frame_start if sequences_frame_start > frame_start else sequences_frame_end
+
+    def remove_gaps(self, sequence_blocks, gap_frame_start, remove_all=False):
         for block in sequence_blocks:
-            print("Block start {!s}, end {!s}, len {!s}".format(block[0].frame_final_start, block[-1].frame_final_end, len(block)))
-
-
-        # FIXME: can't read strips with inputs
-        # Apply the same time offset to each sequence_block
-        current_gap_start_frame = first_gap_frame
-        for sequence_block in sequence_blocks:
-            sorted_block = sorted(sequence_block, key=attrgetter('frame_final_start', 'frame_final_end'))
-            gap_size = sorted_block[0].frame_final_start - current_gap_start_frame
+            gap_size = block[0].frame_final_start - gap_frame_start
             # print("Gap size: %s" % gap_size)
             if gap_size < 1:
                 continue
-
-            for s in sorted_block:
+            for s in block:
                 try:
                     s.frame_start -= gap_size
                 except AttributeError:
                     pass
-            if not self.all:
+            if not self.remove_all:
                 break
-            current_gap_start_frame = sorted_block[-1].frame_final_end
-        return {'FINISHED'}
+            gap_frame_start = block[-1].frame_final_end
+
+    def find_sequences_after_gap(self, sorted_sequences, first_gap_frame):
+        after_gap = []
+        if self.ignore_locked:
+            after_gap = [s for s in sorted_sequences
+                            if s.frame_final_start >= first_gap_frame and
+                            not s.lock]
+        else:
+            after_gap = [s for s in sorted_sequences
+                            if s.frame_final_start >= first_gap_frame]
+        return after_gap
