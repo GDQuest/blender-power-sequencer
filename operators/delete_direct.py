2eb420f9c64013b535b5c879a761a2ed9b0ad259
Nathan Lovato <nathan@gdquest.com>
2018-08-31 10:04:16 +0900

    delete_direct: auto find strip under mouse if none selected
    closes #223

--- operators/delete_direct.py
+++ operators/delete_direct.py
@@ -1,5 +1,7 @@
 import bpy
 from .utils.global_settings import SequenceTypes
+from .utils.find_strips_mouse import find_strips_mouse
+from .utils.get_mouse_view_coords import get_mouse_frame_and_channel
 
 
 class DeleteDirect(bpy.types.Operator):
@@ -9,16 +11,22 @@ class DeleteDirect(bpy.types.Operator):
     bl_description = "Delete without confirmation"
     bl_options = {'REGISTER', 'UNDO'}
 
+    frame, channel = None, None
+
     @classmethod
     def poll(cls, context):
-        return len(bpy.context.selected_sequences) > 0
+        return len(context.sequences) > 0
+
+    def invoke(self, context, event):
+        self.frame, self.channel = get_mouse_frame_and_channel(event)
+        return self.execute(context)
 
     def execute(self, context):
-        selection = bpy.context.selected_sequences
-        selection_length = len(selection)
+        start_selection = self.get_start_selection()
+        selection_length = len(start_selection)
 
         bpy.ops.power_sequencer.crossfade_remove()
-        for s in selection:
+        for s in start_selection:
             try:
                 s.select = True
             except:
@@ -29,3 +37,17 @@ class DeleteDirect(bpy.types.Operator):
         report_message += 's' if selection_length > 1 else ''
         self.report({'INFO'}, report_message)
         return {"FINISHED"}
+
+    def get_start_selection(self):
+        """
+        Returns a list of strips to use with the delete operator
+        If there's no selected_sequences, finds linked strips under the mouse cursor
+        """
+        strips = bpy.context.selected_sequences
+        if strips == []:
+            if self.frame is None:
+                return []
+            strips = find_strips_mouse(self.frame, self.channel, select_linked=True)
+            for s in strips:
+                s.select = True
+        return strips
--- operators/utils/find_strips_mouse.py
+++ operators/utils/find_strips_mouse.py
@@ -1,7 +1,7 @@
 import bpy
 
 
-def find_strips_mouse(frame=None, channel=None, select_linked=False):
+def find_strips_mouse(frame, channel, select_linked=False):
     """
     Finds a list of sequences to select based on the mouse position
     or using the time cursor.
@@ -9,25 +9,21 @@ def find_strips_mouse(frame=None, channel=None, select_linked=False):
     Args:
     - frame: the frame the mouse or cursor is on
     - channel: the channel the mouse is hovering
-    - select_linked: include the sequences linked in time if True
+    - select_linked: find and append the sequences linked in time if True
 
     Returns the sequence(s) under the mouse cursor as a list
     Returns an empty list if nothing found
     """
-    sequences = bpy.context.sequences
-    selection = []
+    sequences = [s for s in bpy.context.sequences if not s.lock and s.channel == channel]
+    try:
+        under_mouse = list(next(s for s in sequences if s.frame_final_start <= frame <= s.frame_final_end))
+    except StopIteration:
+        return []
 
-    for s in sequences:
-        if s.lock or (not s.channel == channel):
-            continue
-        if s.frame_final_start <= frame <= s.frame_final_end:
-            selection.append(s)
-            break
-
-    if select_linked and len(selection) > 0:
-        for s in sequences:
-            if s.channel == selection[0].channel or s.lock:
-                continue
-            if s.frame_final_start == selection[0].frame_final_start and s.frame_final_end == selection[0].frame_final_end:
-                selection.append(s)
-    return selection
+    if select_linked:
+        linked_strips = [s for s in sequences if
+                         s.frame_final_start == under_mouse[0].frame_final_start
+                         and s.frame_final_end == under_mouse[0].frame_final_end]
+        return under_mouse.append(linked_strips)
+    else:
+        return under_mouse
