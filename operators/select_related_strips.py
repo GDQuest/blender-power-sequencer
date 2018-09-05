3a63a71d249264399eaa8628bc12e0c5c40f0c05
Nathan Lovato <nathan@gdquest.com>
2018-09-06 08:26:12 +0900

    Improve select related: find effects recursively

--- operators/select_related_strips.py
+++ operators/select_related_strips.py
@@ -17,26 +17,53 @@ class SelectRelatedStrips(bpy.types.Operator):
         return len(context.selected_sequences) > 0
 
     def execute(self, context):
-        effects = [s for s in bpy.context.sequences
+        effects = [s for s in context.sequences
                    if s.type in SequenceTypes.EFFECT]
+        found_effects = self.find_related_effects(context.selected_sequences, effects)
+        effects_to_select = list(found_effects)
+        while len(found_effects) > 0:
+            found_effects = self.find_related_effects(found_effects, effects)
+            effects_to_select.extend(found_effects)
 
-        selected_effects = [s for s in bpy.context.selected_sequences
-                            if s.type in SequenceTypes.EFFECT]
-        for e in selected_effects:
-            e.input_1.select = True
-            try:
-                e.input_2.select = True
-            except Exception:
-                pass
+        for e in effects_to_select:
+            e.select = True
 
-        for s in bpy.context.selected_sequences:
+        # found_inputs = self.find_related_inputs(context.selected_sequences, effects)
+        # inputs_to_select = list(found_inputs)
+        # while len(found_inputs) > 0:
+        #     found_inputs = self.find_related_inputs(found_inputs)
+        #     inputs_to_select.extend(found_inputs)
+
+        # for i in inputs_to_select:
+        #     i.select = True
+        return {'FINISHED'}
+
+    def find_related_effects(self, sequences, effects):
+        found = []
+        for s in sequences:
             for e in effects:
-                if e.input_1 == s:
-                    e.select = True
+                try:
+                    if e.input_1 == s:
+                        found.append(e)
+                except Exception:
                     continue
                 try:
                     if e.input_2 == s:
-                        e.select = True
+                        found.append(e)
                 except Exception:
                     continue
-        return {'FINISHED'}
+        return found
+
+    # Won't work: you need to only select inputs related to the starting selection
+    # def find_related_inputs(self, sequences, effects):
+    #     found_inputs = []
+    #     for e in effects:
+    #         try:
+    #             found_inputs.append(e.input_1)
+    #         except Exception:
+    #             pass
+    #         try:
+    #             found_inputs.append(e.input_2)
+    #         except Exception:
+    #             pass
+    #     return found_inputs
