0a394eddf4d2c95af59a3763d11449beda1aa76c
Nathan Lovato <nathan@gdquest.com>
2018-09-07 16:11:06 +0900

    Add optional support for simple select related back

--- operators/select_related_strips.py
+++ operators/select_related_strips.py
@@ -1,5 +1,6 @@
 import bpy
 from operator import attrgetter
+from .utils.global_settings import SequenceTypes
 
 class SelectRelatedStrips(bpy.types.Operator):
     """
@@ -12,21 +13,36 @@ class SelectRelatedStrips(bpy.types.Operator):
     bl_description = "Find and select all strips related to the selection"
     bl_options = {'REGISTER', 'UNDO'}
 
+    find_all = bpy.props.BoolProperty(
+        name="Find All",
+        description="Find all related strips recursively so that you can copy the selection without getting an error from Blender",
+        default=True
+    )
+
     @classmethod
     def poll(cls, context):
         return len(context.selected_sequences) > 0
 
     def execute(self, context):
-        related_strips = set()
-        for s in context.selected_sequences:
-            self.dfs(related_strips, s, context)
-            
+        if self.find_all:
+            related_strips = set()
+            for s in context.selected_sequences:
+                self.find_neighbours_recursive(related_strips, s, context)
+        else:
+            related_strips = []
+            # Only select direct neighbours and attached effects
+            effects = [s for s in context.sequences
+                    if s.type in SequenceTypes.EFFECT]
+            found_effects = self.find_related_effects(context.selected_sequences, effects)
+            related_strips.extend(found_effects)
+            while len(found_effects) > 0:
+                found_effects = self.find_related_effects(found_effects, effects)
+                related_strips.extend(found_effects)
         for s in related_strips:
             s.select = True
-        
         return {'FINISHED'}
-    
-    def dfs(self, visited, strip, context):
+
+    def find_neighbours_recursive(self, visited, strip, context):
         """
         Performs a depth first search traversal to the graph of strips, to find
         all related strips.
@@ -38,25 +54,25 @@ class SelectRelatedStrips(bpy.types.Operator):
         neighbours = self.find_neighbours(strip, context)
         for s in neighbours:
             if s not in visited:
-                self.dfs(visited, s, context)
-    
+                self.find_neighbours_recursive(visited, s, context)
+
     def find_neighbours(self, strip, context):
         """
-        Strips and their effect strips define a graph, where each node is a 
-        strip and edges are their connections. It finds all the neighbours of a 
+        Strips and their effect strips define a graph, where each node is a
+        strip and edges are their connections. It finds all the neighbours of a
         strip in the graph, and *sometimes neighbours of neighbours and so on*.
-        *In order to find the neighbours of a strip the 
+        *In order to find the neighbours of a strip the
         bpy.ops.transform.seq_slide operator is used, and usually finds many
         levels of neighbours, but always finds the first level, which is needed,
-        the other levels are redundant, but are included for brevity reasons. 
+        the other levels are redundant, but are included for brevity reasons.
         Args:
         - strip: The strip to find all its neighbours.
-        Returns: A list with all the neighbours of the strip and sometimes 
+        Returns: A list with all the neighbours of the strip and sometimes
                  neighbours of neighbours and so on.
         """
         #Respects initial selection
         init_selected_strips = [s for s in context.selected_sequences]
-        
+
         neighbours = []
         bpy.ops.sequencer.select_all(action='DESELECT')
         strip.select = True
@@ -64,15 +80,31 @@ class SelectRelatedStrips(bpy.types.Operator):
         strip.select = False
         for s in context.selected_sequences:
             neighbours.append(s)
-                
+
         try:
             neighbours.append(strip.input_1)
             neighbours.append(strip.input_2)
         except Exception:
             pass
-        
+
         bpy.ops.sequencer.select_all(action='DESELECT')
         for s in init_selected_strips:
             s.select = True
-            
+
         return neighbours
+
+    def find_related_effects(self, sequences, effects):
+        found = []
+        for s in sequences:
+            for e in effects:
+                try:
+                    if e.input_1 == s:
+                        found.append(e)
+                except Exception:
+                    continue
+                try:
+                    if e.input_2 == s:
+                        found.append(e)
+                except Exception:
+                    continue
+        return found
