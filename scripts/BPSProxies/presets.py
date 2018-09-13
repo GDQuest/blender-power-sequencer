ea55d51384dc1fb8044418c17231a0a0ae316466
Francois Grassard <francois@coyhot.com>
2018-09-13 09:42:40 +0900

    Add default FFMPEG presets, include link to Francois's explanations

---
+++ scripts/BPSProxies/presets.py
@@ -0,0 +1,29 @@
+"""
+Presets provided by François Grassard. Reference commands:
+    ffmpeg -i input.mov -pix_fmt yuv420p -crf 25 -c:v libvpx -speed 16 \
+    -g 1 -threads 8 -vf colormatrix=bt601:bt709 output.webm
+    ffmpeg -i input.mov -pix_fmt yuv420p -crf 25 -preset veryslow \
+    -g 1 -vf colormatrix=bt601:bt709 output.mp4
+
+For detailed information, see:
+https://github.com/GDquest/Blender-power-sequencer/issues/174#issuecomment-420586813
+
+"""
+MP4_DEFAULTS = {
+    'format': 'mp4',
+    'codec': 'libx264',
+    'crf': 25,
+    'encoding_speed': {
+        'key': "preset",
+        'value': "faster",
+    },
+}
+WEBM_DEFAULTS = {
+    'format': 'webm',
+    'codec': "libvpx",
+    'crf': 25,
+    'encoding_speed': {
+        'key': "speed",
+        'value': 16,
+    },
+}
