5d45de1923f9f9c5187d9fbf0ce5265e77797218
Nathan Lovato <nathan@gdquest.com>
2018-09-20 08:40:02 +0900

    Add icons to the toolbar menu
    Regorganize some menu items. Closes #247
    
    Co-authored-by: Libre Artist <libreartist@tutanota.com>

--- ui/menu_toolbar.py
+++ ui/menu_toolbar.py
@@ -7,22 +7,21 @@ class PowerSequencerMenuToolbar(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.synchronize_titles")
-        layout.operator("power_sequencer.set_timeline_range")
-        layout.operator("power_sequencer.cycle_scenes")
-        layout.operator("power_sequencer.channel_offset")
+        layout.operator("power_sequencer.synchronize_titles", icon='SORTALPHA')
+        layout.operator("power_sequencer.set_timeline_range", icon='ARROW_LEFTRIGHT')
+        layout.operator("power_sequencer.cycle_scenes", icon='SCENE')
 
         layout.separator()
 
-        layout.menu("PowerSequencer_MT_trim")
-        layout.menu("PowerSequencer_MT_strips")
-        layout.menu("PowerSequencer_MT_select")
-        layout.menu("PowerSequencer_MT_render")
-        layout.menu("PowerSequencer_MT_playback")
-        layout.menu("PowerSequencer_MT_markers")
-        layout.menu("PowerSequencer_MT_file")
-        layout.menu("PowerSequencer_MT_edit")
-        layout.menu("PowerSequencer_MT_audio")
+        layout.menu("PowerSequencer_MT_render", icon='RENDER_ANIMATION')
+        layout.menu("PowerSequencer_MT_playback", icon='PLAY')
+        layout.menu("PowerSequencer_MT_trim", icon='LINE_DATA')
+        layout.menu("PowerSequencer_MT_strips", icon='NLA')
+        layout.menu("PowerSequencer_MT_markers", icon='MARKER')
+        layout.menu("PowerSequencer_MT_audio", icon='SOUND')
+        layout.menu("PowerSequencer_MT_select", icon='RESTRICT_SELECT_OFF')
+        layout.menu("PowerSequencer_MT_edit", icon='MODIFIER')
+        layout.menu("PowerSequencer_MT_file", icon='FILE_FOLDER')
 
 
 class PowerSequencer_MT_playback(bpy.types.Menu):
@@ -44,15 +43,21 @@ class PowerSequencer_MT_strips(bpy.types.Menu):
 
         layout.separator()
 
-        layout.menu("PowerSequencer_MT_strips_speed")
-        layout.operator("power_sequencer.concatenate_strips")
-        layout.operator("power_sequencer.fade_strips")
-        layout.operator("power_sequencer.swap_strips")
-        layout.operator("power_sequencer.toggle_selected_mute")
+        layout.menu("PowerSequencer_MT_strips_speed", icon='TIME')
+        layout.operator("power_sequencer.concatenate_strips", icon='CONSTRAINT')
+        layout.operator("power_sequencer.fade_strips", icon='IMAGE_RGB_ALPHA')
+        layout.operator("power_sequencer.swap_strips", icon='ARROW_LEFTRIGHT')
+        layout.operator("power_sequencer.toggle_selected_mute", icon='MUTE_IPO_ON')
+        layout.operator("power_sequencer.channel_offset", icon='AUTOMERGE_ON')
 
         layout.separator()
 
-        layout.menu("PowerSequencer_MT_strips_fades")
+        layout.menu("PowerSequencer_MT_crossfade", icon='IMAGE_RGB_ALPHA')
+        layout.menu("PowerSequencer_MT_strips_fades", icon='IMAGE_RGB_ALPHA')
+
+        layout.separator()
+
+        layout.operator("power_sequencer.make_still_image", icon='FILE_IMAGE')
 
 
 class PowerSequencer_MT_strips_speed (bpy.types.Menu):
@@ -78,14 +83,14 @@ class PowerSequencer_MT_select(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.border_select")
-        layout.operator("power_sequencer.select_linked_effect")
-        layout.menu("PowerSequencer_MT_deselect")
+        layout.operator("power_sequencer.border_select", icon='BORDER_RECT')
+        layout.operator("power_sequencer.select_linked_effect", icon='CONSTRAINT')
+        layout.menu("PowerSequencer_MT_deselect", icon='RESTRICT_SELECT_ON')
 
         layout.separator()
 
-        layout.menu("PowerSequencer_MT_grab")
-        layout.menu("PowerSequencer_MT_snap")
+        layout.menu("PowerSequencer_MT_grab", icon='HAND')
+        layout.menu("PowerSequencer_MT_snap", icon='SNAP_ON')
 
 
 class PowerSequencer_MT_deselect(bpy.types.Menu):
@@ -121,12 +126,12 @@ class PowerSequencer_MT_edit(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.menu("PowerSequencer_MT_delete")
+        layout.menu("PowerSequencer_MT_delete", icon='X')
 
         layout.separator()
 
-        layout.operator("power_sequencer.remove_gaps")
-        layout.operator("power_sequencer.copy_selected_sequences")
+        layout.operator("power_sequencer.remove_gaps", icon='FRAME_PREV')
+        layout.operator("power_sequencer.copy_selected_sequences", icon='COPYDOWN')
 
 
 class PowerSequencer_MT_delete(bpy.types.Menu):
@@ -143,15 +148,15 @@ class PowerSequencer_MT_markers(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.menu("PowerSequencer_MT_marker_delete")
+        layout.menu("PowerSequencer_MT_marker_delete", icon='X')
 
         layout.separator()
 
-        layout.operator("power_sequencer.go_to_next_marker")
+        layout.operator("power_sequencer.go_to_next_marker", icon='TRIA_RIGHT_BAR')
         layout.operator("power_sequencer.markers_as_timecode")
         layout.operator("power_sequencer.snap_marker_to_cursor")
         layout.operator("power_sequencer.set_preview_between_markers")
-        layout.operator("power_sequencer.markers_snap_matching_strips")
+        layout.operator("power_sequencer.markers_snap_matching_strips", icon='NLA')
 
 
 class PowerSequencer_MT_marker_delete(bpy.types.Menu):
@@ -168,9 +173,7 @@ class PowerSequencer_MT_render(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.make_still_image")
         layout.operator("power_sequencer.render_for_web")
-
 
 
 class PowerSequencer_MT_file(bpy.types.Menu):
@@ -178,10 +181,10 @@ class PowerSequencer_MT_file(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.open_project_directory")
-        layout.operator("power_sequencer.save_direct")
-        layout.operator("power_sequencer.import_local_footage")
-        layout.operator("power_sequencer.set_video_proxies")
+        layout.operator("power_sequencer.open_project_directory", icon='FILE_FOLDER')
+        layout.operator("power_sequencer.save_direct", icon='FILE_TICK')
+        layout.operator("power_sequencer.import_local_footage", icon='FILE_MOVIE')
+        layout.operator("power_sequencer.set_video_proxies", icon='EXTERNAL_DATA')
 
 
 class PowerSequencer_MT_trim(bpy.types.Menu):
@@ -189,13 +192,13 @@ class PowerSequencer_MT_trim(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.trim_three_point_edit")
-        layout.operator("power_sequencer.trim_to_surrounding_cuts")
+        layout.operator("power_sequencer.trim_three_point_edit", icon='TRIA_DOWN')
+        layout.operator("power_sequencer.trim_to_surrounding_cuts", icon='SNAP_PEEL_OBJECT')
 
         layout.separator()
-        layout.menu("PowerSequencer_MT_mouse")
+        layout.menu("PowerSequencer_MT_mouse", icon='RESTRICT_SELECT_OFF')
         layout.separator()
-        layout.menu("PowerSequencer_MT_preview")
+        layout.menu("PowerSequencer_MT_preview", icon='SEQ_PREVIEW')
 
 
 class PowerSequencer_MT_mouse(bpy.types.Menu):
@@ -221,13 +224,9 @@ class PowerSequencer_MT_audio(bpy.types.Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("power_sequencer.align_audios")
-        layout.operator("power_sequencer.toggle_waveforms")
-        layout.operator("power_sequencer.mouse_toggle_mute")
-
-        layout.separator()
-
-        layout.menu("PowerSequencer_MT_crossfade")
+        layout.operator("power_sequencer.align_audios", icon='ALIGN')
+        layout.operator("power_sequencer.toggle_waveforms", icon='RNDCURVE')
+        layout.operator("power_sequencer.mouse_toggle_mute", icon='MUTE_IPO_ON')
 
 
 class PowerSequencer_MT_crossfade(bpy.types.Menu):
