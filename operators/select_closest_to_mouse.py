6003ab32f48a6037ffaa810f7a36fe5f2853cdae
Nathan Lovato <nathan@gdquest.com>
2018-10-17 08:28:25 +0900

    Make select_closest_to_mouse callable from other operators

--- operators/select_closest_to_mouse.py
+++ operators/select_closest_to_mouse.py
@@ -8,12 +8,21 @@ class SelectClosestToMouse(bpy.types.Operator):
     bl_description = 'Select the closest strip under the mouse cursor'
     bl_options = {'REGISTER', 'UNDO'}
 
+    frame = bpy.props.IntProperty(name="Frame")
+    channel = bpy.props.IntProperty(name="Channel")
+
     @classmethod
     def poll(cls, context):
         return True
 
     def invoke(self, context, event):
-        frame, channel = get_mouse_frame_and_channel(event)
-        strip = find_strips_mouse(frame, channel)[0]
-        strip.select = True
+        self.frame, self.channel = get_mouse_frame_and_channel(event)
+        return self.execute(context)
+
+    def execute(self, context):
+        try:
+            strip = find_strips_mouse(self.frame, self.channel)[0]
+            strip.select = True
+        except Exception:
+            return {"CANCELLED"}
         return {"FINISHED"}
