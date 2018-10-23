deb1e3334fe484bb2595bc09ec59690168f91bba
Nathan Lovato <nathan@gdquest.com>
2018-10-23 11:54:12 +0900

    Remove obsolete set_video_proxies, fix #263

--- addon_preferences.py
+++ addon_preferences.py
@@ -8,31 +8,10 @@ from . import addon_updater_ops
 
 class ProxyPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
-
     video_export_path = StringProperty(
         name="Video render folder",
         description="Relative folder to save videos rendered with the add-on",
         default="")
-    auto_render_proxies = BoolProperty(
-        name="Create proxies automatically",
-        description="Automatically build proxies for video sequences on import",
-        default=False)
-    use_custom_folder = BoolProperty(
-        name="Custom proxy folder",
-        description="Use a specific folder to store proxies",
-        default=False)
-    custom_folder_path = StringProperty(
-        name="Custom proxy folder path",
-        description="Store the generated proxies in a specific folder on your hard drive (absolute path)",
-        default=r"D:\Program Files\Blender proxies")
-
-    proxy_25 = BoolProperty(name="Proxy at 25%", default=True)
-    proxy_50 = BoolProperty(name="Proxy at 50%", default=False)
-    proxy_75 = BoolProperty(name="Proxy at 75%", default=False)
-    proxy_100 = BoolProperty(name="Proxy at 100%", default=False)
-    proxy_quality = IntProperty(
-        name="Proxy JPG quality",
-        default=90, min=1, max=100)
 
     # addon updater preferences
     auto_check_update = bpy.props.BoolProperty(
@@ -72,9 +51,6 @@ class ProxyPreferences(bpy.types.AddonPreferences):
         layout = self.layout
 
         layout.prop(self, "video_export_path")
-        layout.prop(self, "auto_render_proxies")
-        layout.prop(self, "use_custom_folder")
-        layout.prop(self, "custom_folder_path")
 
         # updater draw function
         addon_updater_ops.update_settings_ui(self, context)
