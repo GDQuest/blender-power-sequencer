d5cde82b83f5e49314f69e2e94b9122952341cd4
samytichadou <samytichadou@gmail.com>
2018-11-10 17:27:15 +0100

    Update select_playing.py

--- operators/select_playing.py
+++ operators/select_playing.py
@@ -17,34 +17,7 @@ class SelectPlaying(bpy.types.Operator):
         FuncSelectPlaying(self.all)
         return {"FINISHED"}
     
-# select playing Menu
-class SelectPlayingMenu(bpy.types.Operator):
-    bl_idname = "power_sequencer.select_playing_menu"
-    bl_label = "Select Playing Clips Menu"
-    bl_description = ""
-    bl_options = {"REGISTER", "UNDO"}
-    
-    all=bpy.props.BoolProperty()
 
-    @classmethod
-    def poll(cls, context):
-        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
-    
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_props_dialog(self, width=300, height=100)
-    
-    def check(self, context):
-        return True
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, 'all')
-
-    def execute(self, context):
-        FuncSelectPlaying(self.all)
-        return {"FINISHED"}
-    
 # select playing function
 def FuncSelectPlaying(all):
     scn=bpy.context.scene
