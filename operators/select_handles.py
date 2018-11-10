24f4432f94763c86183bbcd68a108d9c6d2aeadd
samytichadou <samytichadou@gmail.com>
2018-11-10 17:25:55 +0100

    Update select_handles.py

--- operators/select_handles.py
+++ operators/select_handles.py
@@ -21,38 +21,7 @@ class SelectHandles(bpy.types.Operator):
         FuncSelectHandles(self.right)
         return {"FINISHED"}
     
-# select handles menu
-class SelectHandlesMenu(bpy.types.Operator):
-    bl_idname = "power_sequencer.select_handles_menu"
-    bl_label = "Select Handles"
-    bl_description = ""
-    bl_options = {"REGISTER", "UNDO"}
-    
-    right=bpy.props.BoolProperty(name="Right")
 
-    @classmethod
-    def poll(cls, context):
-        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
-    
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_props_dialog(self, width=300, height=100)
-    
-    def check(self, context):
-        return True
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, 'right')
-
-    def execute(self, context):
-        if self.right==True:
-            bpy.ops.sequencer.strip_jump(next=True, center=False)
-        else:
-            bpy.ops.sequencer.strip_jump(next=False, center=False)
-        FuncSelectHandles(self.right)
-        return {"FINISHED"}
-    
 # select handles function
 def FuncSelectHandles(right):
     scn=bpy.context.scene
