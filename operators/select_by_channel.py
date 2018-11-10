00ff858b9c702475518a7cb5afeb4b109b1139e2
samytichadou <samytichadou@gmail.com>
2018-11-10 13:07:17 +0100

    Create select_by_channel.py

---
+++ operators/select_by_channel.py
@@ -0,0 +1,76 @@
+import bpy
+
+class BlenderEditSelectChannel(bpy.types.Operator):
+    bl_idname = "blenderedit.select_channel"
+    bl_label = "Select Channel"
+    bl_description = ""
+    bl_options = {"REGISTER", "UNDO"}
+    
+    add=bpy.props.BoolProperty()
+    channel=bpy.props.IntProperty(min=1, max=32)
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
+    
+    def execute(self, context):
+        SelectByChannel(self.add, self.channel)
+        return {"FINISHED"}
+    
+# select by channel menu
+class BlenderEditSelectChannelMenu(bpy.types.Operator):
+    bl_idname = "blenderedit.select_channel_menu"
+    bl_label = "Select Channel"
+    bl_description = ""
+    bl_options = {"REGISTER", "UNDO"}
+    
+    add=bpy.props.BoolProperty()
+    channel=bpy.props.IntProperty(min=1, max=32)
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self, width=300, height=100)
+    
+    def check(self, context):
+        return True
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, 'add')
+        layout.prop(self, 'channel')
+
+    def execute(self, context):
+        SelectByChannel(self.add, self.channel)
+        return {"FINISHED"}
+    
+# select by channel function
+def SelectByChannel(add, channel):
+    scn=bpy.context.scene
+    channelstrip=[]
+    chk=0
+    for s in scn.sequence_editor.sequences_all:
+        if add==False:
+            s.select=False
+            s.select_left_handle=False
+            s.select_right_handle=False
+        if s.channel==channel:
+            if add==False:
+                s.select=True
+            else:
+                channelstrip.append(s)
+                if s.select==True:
+                    chk=1
+    if add==True:
+        if chk==0:
+            for s in channelstrip:
+                s.select=True
+        else:
+            for s in channelstrip:
+                s.select=False
+                s.select_left_handle=False
+                s.select_right_handle=False
+    return {"FINISHED"}
