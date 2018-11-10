9cba315e412bd6da8312f2a6be191e5dddd203ec
samytichadou <samytichadou@gmail.com>
2018-11-10 17:19:19 +0100

    add go to in out menu

--- operators/go_to_in_out_selection.py
+++ operators/go_to_in_out_selection.py
@@ -2,7 +2,7 @@ import bpy
 
 # go to in out
 class GoToInOut(bpy.types.Operator):
-    bl_idname = "powersequencer.go_to_in_out"
+    bl_idname = "power_sequencer.go_to_in_out"
     bl_label = "Go to Strip In Out"
     bl_description = ""
     bl_options = {"REGISTER", "UNDO"}
@@ -17,6 +17,33 @@ class GoToInOut(bpy.types.Operator):
         FuncGoToInOut(self.out)
         return {"FINISHED"}
     
+# go to in out Menu
+class GoToInOutMenu(bpy.types.Operator):
+    bl_idname = "power_sequencer.go_to_in_out_menu"
+    bl_label = "Go to Strip In Out Menu"
+    bl_description = ""
+    bl_options = {"REGISTER", "UNDO"}
+    
+    out=bpy.props.BoolProperty(name="Out")
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self, width=300, height=100)
+    
+    def check(self, context):
+        return True
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, 'out')
+
+    def execute(self, context):
+        FuncGoToInOut(self.out)
+        return {"FINISHED"}    
     
 # go to in out function
 def FuncGoToInOut(out):
