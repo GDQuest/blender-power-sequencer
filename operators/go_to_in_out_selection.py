7a861ba152709eb79e036aff8b86ddb36b4cdce9
samytichadou <samytichadou@gmail.com>
2018-11-10 17:20:05 +0100

    Update go_to_in_out_selection.py

--- operators/go_to_in_out_selection.py
+++ operators/go_to_in_out_selection.py
@@ -17,34 +17,6 @@ class GoToInOut(bpy.types.Operator):
         FuncGoToInOut(self.out)
         return {"FINISHED"}
     
-# go to in out Menu
-class GoToInOutMenu(bpy.types.Operator):
-    bl_idname = "power_sequencer.go_to_in_out_menu"
-    bl_label = "Go to Strip In Out Menu"
-    bl_description = ""
-    bl_options = {"REGISTER", "UNDO"}
-    
-    out=bpy.props.BoolProperty(name="Out")
-
-    @classmethod
-    def poll(cls, context):
-        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
-    
-    def invoke(self, context, event):
-        wm = context.window_manager
-        return wm.invoke_props_dialog(self, width=300, height=100)
-    
-    def check(self, context):
-        return True
-    
-    def draw(self, context):
-        layout = self.layout
-        layout.prop(self, 'out')
-
-    def execute(self, context):
-        FuncGoToInOut(self.out)
-        return {"FINISHED"}    
-    
 # go to in out function
 def FuncGoToInOut(out):
     scn=bpy.context.scene
