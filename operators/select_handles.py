930da4947f2ca9b033c0ca3f5637d93f604286b8
samytichadou <samytichadou@gmail.com>
2018-11-10 17:14:55 +0100

    add select handles with menu

--- operators/select_handles.py
+++ operators/select_handles.py
@@ -2,7 +2,7 @@ import bpy
 
 # select handles
 class SelectHandles(bpy.types.Operator):
-    bl_idname = "powersequencer.select_handles"
+    bl_idname = "power_sequencer.select_handles"
     bl_label = "Select Handles"
     bl_description = ""
     bl_options = {"REGISTER", "UNDO"}
@@ -12,6 +12,38 @@ class SelectHandles(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
+
+    def execute(self, context):
+        if self.right==True:
+            bpy.ops.sequencer.strip_jump(next=True, center=False)
+        else:
+            bpy.ops.sequencer.strip_jump(next=False, center=False)
+        FuncSelectHandles(self.right)
+        return {"FINISHED"}
+    
+# select handles menu
+class SelectHandlesMenu(bpy.types.Operator):
+    bl_idname = "power_sequencer.select_handles_menu"
+    bl_label = "Select Handles"
+    bl_description = ""
+    bl_options = {"REGISTER", "UNDO"}
+    
+    right=bpy.props.BoolProperty(name="Right")
+
+    @classmethod
+    def poll(cls, context):
+        return bpy.context.area.type=='SEQUENCE_EDITOR' and bpy.context.scene.sequence_editor is not None
+    
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self, width=300, height=100)
+    
+    def check(self, context):
+        return True
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, 'right')
 
     def execute(self, context):
         if self.right==True:
