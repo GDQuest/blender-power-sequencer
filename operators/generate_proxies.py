0387dff244b4a77632743a223f3bdd603c60846b
Davide Cristini <davcri91@gmail.com>
2019-01-26 15:48:03 +0100

    Add wip code to use modal operator for completion progress.
    There are currently some issues:
    - There is still no data exchange between bpsproxy and the plugin (the
    progress is just a timer
    - You need to have bpsproxy in the PATH environment varialbe to make
    Blender run it. No warning is presented to the user at the moment.

--- operators/generate_proxies.py
+++ operators/generate_proxies.py
@@ -8,8 +8,7 @@ from .utils.doc import doc_name, doc_idname, doc_brief, doc_description
 
 class GenerateProxies(bpy.types.Operator):
     """
-    Generate proxies using `bpsproxy` script. It requires `bpsproxy` 
-    installed and in the PATH environment variable.
+    Generate proxies using `bpsproxy` script.
     """
     doc = {
         'name': doc_name(__qualname__),
@@ -27,12 +26,15 @@ class GenerateProxies(bpy.types.Operator):
     bl_description = doc_brief(doc['description'])
     bl_options = {'REGISTER', 'UNDO'}
     SEQUENCER_AREA = None
-
     videos_path = bpy.props.StringProperty()
+
+    _timer = None
+    running = False
+    fake_progress = 0
 
     @classmethod
     def poll(cls, context):
-        return len(context.selected_sequences) > 0
+        return GenerateProxies.running == False
 
     def execute(self, context):
         if not bpy.data.is_saved:
@@ -40,16 +42,30 @@ class GenerateProxies(bpy.types.Operator):
                 {'ERROR_INVALID_INPUT'},
                 'You need to save your project first. Proxies generation cancelled.')
             return {'CANCELLED'}
+        # start bpsproxy in the background
+        self.run_bpsproxy(context)
+        # Run the operator in "modal" mode, to avoid crashes
+        # read more here: https://blender.stackexchange.com/questions/1050/blender-ui-multithreading-progressbar
+        wm = context.window_manager
+        wm.modal_handler_add(self)
+        wm.progress_begin(0, 100)
+        # start timer to trigger modal events
+        self._timer = wm.event_timer_add(0.25, context.window)
+        # update Class variable to keep track of the state
+        GenerateProxies.running = True
+        return {'RUNNING_MODAL'}
 
-        video_directory_path = bpy.path.abspath(context.scene.video_directory)
-
+    def run_bpsproxy(self, context):
         # TODO: check if bpsproxy is installed
-        # eg: Blender does not take environment variable from user configuration like .bashrc or .zshrc
-        # I have ~/.local/bin exported in my zshrc file and I can use bpsproxy from the console, but Blender doesn't
+        # eg: Blender does not take environment variable from 
+        # user configuration like .bashrcor .zshrc
+        # I have ~/.local/bin exported in my zshrc file and I can 
+        # use bpsproxy from the console but Blender doesn't,
         # unless executed from the console.
-        # 
         # https://unix.stackexchange.com/questions/81243/how-do-i-set-the-path-or-other-environment-variables-so-that-x-apps-can-access-i
         subprocess.call(["printenv", "PATH"])
+
+        video_directory_path = bpy.path.abspath(context.scene.video_directory)
         bpsproxy_command = ['bpsproxy', video_directory_path]
         sizes = []
 
@@ -64,25 +80,19 @@ class GenerateProxies(bpy.types.Operator):
         if context.scene.proxy_preset:
             bpsproxy_command.extend(['-p', context.scene.proxy_preset])
 
-        # debug print
-        print(bpsproxy_command)
-
-        # TODO: progress update
-        wm = bpy.context.window_manager
-        tot = 10000000
-        wm.progress_begin(0, tot)
-        update_gui_thread = threading.Thread(target=update_progress, args=(wm, tot))
-        update_gui_thread.start()
-
-        execute_bpsproxy = lambda shell_cmd: subprocess.Popen(shell_cmd)
-        bpsproxy_thread = threading.Thread(target=execute_bpsproxy, args=([bpsproxy_command]))
-        bpsproxy_thread.start()
+        subprocess.Popen(bpsproxy_command)
 
         return {'FINISHED'}
 
-
-def update_progress(wm, tot):
-    import time
-    for i in range(tot):
-        wm.progress_update(i)
-    wm.progress_end()
+    def modal(self, context, event):
+        if event.type == "TIMER":
+            wm = context.window_manager
+            if GenerateProxies.fake_progress < 100:
+                GenerateProxies.fake_progress += 1
+                wm.progress_update(GenerateProxies.fake_progress)
+            else:
+                wm.progress_end()
+                GenerateProxies.running = False 
+                GenerateProxies.fake_progress = 0
+                return {'FINISHED'}
+        return {'PASS_THROUGH'}
