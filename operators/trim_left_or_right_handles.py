2e0aa48c20da9a8be8f3f12886ab348887bf2da3
Guilherme Oliveira <htoguilherme@gmail.com>
2019-03-15 15:38:57 -0300

    Fixed ripple behavior, switched enum prop to bool

--- operators/trim_left_or_right_handles.py
+++ operators/trim_left_or_right_handles.py
@@ -1,6 +1,7 @@
 import bpy
 
 from .utils.doc import doc_name, doc_idname, doc_brief, doc_description
+from operator import attrgetter
 
 
 class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
@@ -12,10 +13,10 @@ class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
         'demo': '',
         'description': doc_description(__doc__),
         'shortcuts': [
-            ({'type': 'K', 'value': 'PRESS', 'alt': True}, {'side': 'right', 'ripple': 'dont_use'}, 'Smart Snap Right'),
-            ({'type': 'K', 'value': 'PRESS', 'alt': True, 'shift': True}, {'side': 'right', 'ripple': 'use'}, 'Smart Snap Right With Ripple'),
-            ({'type': 'K', 'value': 'PRESS', 'ctrl': True}, {'side': 'left', 'ripple': 'dont_use'}, 'Smart Snap Left'),
-            ({'type': 'K', 'value': 'PRESS', 'ctrl': True, 'shift': True}, {'side': 'left', 'ripple': 'use'}, 'Smart Snap Left With Ripple')
+            ({'type': 'K', 'value': 'PRESS', 'alt': True}, {'side': 'right', 'ripple': False}, 'Smart Snap Right'),
+            ({'type': 'K', 'value': 'PRESS', 'alt': True, 'shift': True}, {'side': 'right', 'ripple': True}, 'Smart Snap Right With Ripple'),
+            ({'type': 'K', 'value': 'PRESS', 'ctrl': True}, {'side': 'left', 'ripple': False}, 'Smart Snap Left'),
+            ({'type': 'K', 'value': 'PRESS', 'ctrl': True, 'shift': True}, {'side': 'left', 'ripple': True}, 'Smart Snap Left With Ripple')
         ],
         'keymap': 'Sequencer'
     }
@@ -31,11 +32,10 @@ class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
         description="Handle side to use for the snap",
         default='auto')
 
-    ripple: bpy.props.EnumProperty(
-        items=[('use', 'Ripple', 'Use ripple'), ('dont_use', "Don't ripple", 'Do not use ripple')],
-        name="Use ripple",
-        description="Determines if sequences should be rippled",
-        default='dont_use')
+    ripple: bpy.props.BoolProperty(
+        name="Ripple",
+        default=False
+    )
 
     @classmethod
     def poll(cls, context):
@@ -43,16 +43,21 @@ class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
 
     def execute(self, context):
         frame_current = context.scene.frame_current
-        self.select_strip_handle(context.selected_sequences, self.side, frame_current)
+        to_ripple = self.select_strip_handle(context.selected_sequences, self.side, frame_current)
+        
         bpy.ops.sequencer.snap(frame=frame_current)
+        
+        if self.ripple:
+            for sequence_to_ripple in to_ripple:
+                sequence = sequence_to_ripple["sequence"]
+                original_frame = sequence_to_ripple["original_frame"]
+                gap = original_frame - sequence.frame_final_end if sequence_to_ripple["side"] == 'RIGHT' else sequence.frame_final_start - original_frame
+                self.ripple_sequences(sequence_to_ripple["sequence"], context, gap, sequence_to_ripple["side"])
+        
+        for sequence in context.selected_sequences:
+            sequence.select_right_handle = False
+            sequence.select_left_handle = False
 
-        if self.ripple == 'use':
-            for s in context.selected_sequences:
-                self.ripple_sequences(s, context)
-
-        for s in context.selected_sequences:
-            s.select_right_handle = False
-            s.select_left_handle = False
         return {"FINISHED"}
 
     def select_strip_handle(self, sequences, side, frame):
@@ -60,6 +65,8 @@ class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
         Select the left or right handles of the strips based on the frame number
         """
         side = side.upper()
+        to_ripple = []
+
         for s in sequences:
             s.select_left_handle = False
             s.select_right_handle = False
@@ -75,19 +82,20 @@ class POWER_SEQUENCER_OT_trim_left_or_right_handles(bpy.types.Operator):
                 s.select = False
             if handle_side:
                 bpy.ops.sequencer.select_handles(side=handle_side)
+                original_frame = s.frame_final_end if handle_side == 'RIGHT' else s.frame_final_start
+                to_ripple.append({"side": handle_side, "original_frame": original_frame, "sequence": s})
+        return to_ripple
 
-    def ripple_sequences(self, sequence, context):
-        gap = 0
-        side = 'RIGHT' if sequence.select_right_handle else 'LEFT'
-        if side == 'RIGHT':
-            gap = sequence.frame_final_start + sequence.frame_duration - sequence.frame_final_end
-        else:
-            gap = abs(sequence.frame_start - sequence.frame_final_start)
-
-        for s in context.sequences:
-            if s == sequence or s.channel != sequence.channel:
+    def ripple_sequences(self, sequence, context, gap, side):
+        """
+        Ripples edit sequences in the same channel of sequence moving them by gap
+        """
+        ordered_sequences = sorted(context.sequences, key=attrgetter('frame_final_start'))
+        for s in ordered_sequences:
+            if s.channel != sequence.channel:
                 continue
-            if side == 'RIGHT' and sequence.frame_final_end < s.frame_final_start:
+            if s == sequence and side == 'LEFT':
                 s.frame_start -= gap
-            elif side == 'LEFT' and sequence.frame_final_start > s.frame_final_end:
-                s.frame_start += gap
+                continue
+            if sequence.frame_final_end < s.frame_final_start:
+                s.frame_start -= gap
