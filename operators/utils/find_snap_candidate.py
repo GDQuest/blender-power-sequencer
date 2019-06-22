4184c4d80ea43484469b3a9b8dbcfd8a592262c7
Nathan Lovato <nathan@gdquest.com>
2019-06-22 17:16:07 +0900

    Refactor function to find snap candidates
    Mainly cleaned up the code and variable names

--- operators/utils/find_snap_candidate.py
+++ operators/utils/find_snap_candidate.py
@@ -1,21 +1,22 @@
 def find_snap_candidate(context, frame=0):
     """
-    Finds and returns the best frame snap candidate around the frame
+    Returns the cut frame closest to the `frame` argument
     """
-    closest_cut_frame = 1000000
+    snap_candidate = 1000000
+
     for s in context.sequences:
         start_to_frame = frame - s.frame_final_start
         end_to_frame = frame - s.frame_final_end
+
         distance_to_start = abs(start_to_frame)
         distance_to_end = abs(end_to_frame)
-        smallest_distance = min(distance_to_start, distance_to_end)
 
-        if smallest_distance == distance_to_start:
-            snap_candidate = frame - start_to_frame
-        else:
-            snap_candidate = frame - end_to_frame
+        candidate = (frame - start_to_frame
+                     if min(distance_to_start, distance_to_end) == distance_to_start
+                     else frame - end_to_frame)
 
-        if abs(frame - snap_candidate) < abs(frame - closest_cut_frame):
-            closest_cut_frame = snap_candidate
-    return closest_cut_frame
+        if abs(frame - candidate) < abs(frame - snap_candidate):
+            snap_candidate = candidate
+
+    return snap_candidate
 
