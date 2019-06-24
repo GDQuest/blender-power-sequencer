030eb5f044bc099b7475d52a06e1598ef40895c0
Nathan Lovato <nathan@gdquest.com>
2019-06-24 11:53:17 +0900

    Fix #369: update doc_name function, add docstrings to utils/doc.py

--- operators/utils/doc.py
+++ operators/utils/doc.py
@@ -1,3 +1,7 @@
+"""
+Utilities to convert operator names and docstrings to human-readable text.
+Used to generate names for Blender's operator search, and to generate Power Sequencer's documentation.
+"""
 import re
 
 
@@ -5,20 +9,35 @@ upper_match = lambda m: m.string
 
 
 def doc_idname(s):
+    """
+    Returns the id_name of the operator to register shortcuts in Blender's keymaps or call from other operators.
+    """
     out = '.'.join(map(str.lower, s.split('_OT_')))
     return out
 
 
 def doc_name(s):
+    """
+    Returns the operator's name in a human readable format for Blender's operator search.
+    Removes POWER_SEQUENCER_OT_ from an operator's identifier
+    and converts it to title case.
+    """
     out = s.split('_OT')[-1]
-    out = re.sub('_[a-z]', lambda m: m[0][1:].upper(), s)
+    out = out.replace("_", " ").lstrip().title()
     return out
 
 
 def doc_brief(s):
+    """
+    Returns the first line of an operator's docstring to use as a summary of how the operator works.
+    The line in question must contain *brief*.
+    """
     return ' '.join(s.split('\n\n')[0].split()[1:]) if s.startswith('*brief*') else s
 
 
 def doc_description(s):
+    """
+    Returns the lines after the brief line in an operator's documentation strings. See doc_brief above.
+    """
     return '\n'.join(map(lambda x: x.strip(), s.split('\n'))).strip()
 
