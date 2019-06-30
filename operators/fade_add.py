2a21382e98d9c4783b83b99d1a62eba4cefefbfe
Nathan Lovato <nathan@gdquest.com>
2019-07-01 08:54:39 +0900

    Refactor fade_add to use Fade objects, fix #375

--- operators/fade_add.py
+++ operators/fade_add.py
@@ -5,20 +5,20 @@ from .utils.convert_duration_to_frames import convert_duration_to_frames
 from .utils.doc import doc_name, doc_idname, doc_brief, doc_description
 
 
-# TODO: Use a handler to auto move the fades with extend
-# and the strips' handles
 class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
     """
-    Animate a strips opacity to zero. By default, the duration of the fade is 0.5 seconds
+    *brief* Animate a strip's opacity to zero.
+
+    By default, the duration of the fade is 0.5 seconds
     """
     doc = {
         'name': doc_name(__qualname__),
         'demo': 'https://i.imgur.com/XoUM2vw.gif',
         'description': doc_description(__doc__),
         'shortcuts': [
-            ({'type': 'F', 'value': 'PRESS', 'alt': True}, {'fade_type': 'RIGHT'}, 'Fade Right'),
-            ({'type': 'F', 'value': 'PRESS', 'ctrl': True}, {'fade_type': 'LEFT'}, 'Fade Left'),
-            ({'type': 'F', 'value': 'PRESS'}, {'fade_type': 'BOTH'}, 'Fade Both')
+            ({'type': 'F', 'value': 'PRESS', 'alt': True}, {'type': 'OUT'}, 'Fade Right'),
+            ({'type': 'F', 'value': 'PRESS', 'ctrl': True}, {'type': 'IN'}, 'Fade Left'),
+            ({'type': 'F', 'value': 'PRESS'}, {'type': 'IN_OUT'}, 'Fade Both')
         ],
         'keymap': 'Sequencer'
     }
@@ -27,23 +27,21 @@ class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
     bl_description = doc_brief(doc['description'])
     bl_options = {'REGISTER', 'UNDO'}
 
-    fade_duration: bpy.props.FloatProperty(
+    duration_seconds: bpy.props.FloatProperty(
         name="Fade Duration",
         description="Duration of the fade in seconds",
-        default=0.5,
-        min=0)
-    fade_type: bpy.props.EnumProperty(
-        items=[('BOTH', 'Fade in and out', 'Fade selected strips in and out'),
-               ('LEFT', 'Fade in', 'Fade in selected strips'),
-               ('RIGHT', 'Fade out', 'Fade out selected strips')],
+        default=1.0,
+        min=0.0)
+    type: bpy.props.EnumProperty(
+        items=[('IN_OUT', 'Fade in and out', 'Fade selected strips in and out'),
+               ('IN', 'Fade in', 'Fade in selected strips'),
+               ('OUT', 'Fade out', 'Fade out selected strips')],
         name="Fade type",
         description="Fade in, out, or both in and out. Default is both",
-        default='BOTH')
+        default='IN_OUT')
 
-    # The properties below mutate for every sequence to apply fades to
-    animated_property = ''
-    # list of Vector()
-    fade_ranges = []
+    # list of Fade objects, mutates for every sequence
+    fades = []
 
     @classmethod
     def poll(cls, context):
@@ -52,8 +50,6 @@ class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
     def execute(self, context):
         scene = context.scene
         fcurves = scene.animation_data.action.fcurves
-
-        self.fade_length = convert_duration_to_frames(context, self.fade_duration)
 
         # We must create a scene action first if there's none
         if not scene.animation_data:
@@ -64,18 +60,13 @@ class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
 
         faded_sequences = []
         for sequence in context.selected_sequences:
+            self.fades = self.calculate_fades(sequence)
+
             if not self.is_long_enough(sequence):
                 continue
 
-            self.animated_property = 'volume' if hasattr(sequence, 'volume') else 'blend_alpha'
-            self.max_value = self.calculate_max_value(sequence)
-            self.fade_ranges = self.calculate_fade_ranges(sequence)
-
-            self.fade_fcurve = self.fade_find_fcurve(context, sequence)
-            if not self.fade_fcurve:
-                self.fade_fcurve = fcurves.new(data_path=sequence.path_from_id(self.animated_property))
-
-            self.fade_animation_clear(context, sequence)
+            self.fade_fcurve = self.fade_find_or_create_fcurve(context, sequence)
+            self.fade_animation_clear(context)
             self.fade_animation_create()
             faded_sequences.append(sequence)
 
@@ -83,26 +74,81 @@ class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
         return {"FINISHED"}
 
     def is_long_enough(self, sequence):
-        minimum_duration = (self.fade_length * 2
-                            if self.fade_type == 'BOTH' else
-                            self.fade_length)
+        duration_frames = self.fades[0].duration_frames
+        minimum_duration = (duration_frames * 2
+                            if self.type == 'IN_OUT' else
+                            duration_frames)
         return sequence.frame_final_duration >= minimum_duration
 
-    def calculate_fade_ranges(self, sequence):
+    def calculate_fades(self, sequence):
         """
-        Returns a dictionary of 1 or 2 tuples of Vectors that represent coordinates to key for fade animations
-        The dictionary has the form {'IN': (start:Vector, end:Vector), 'OUT': (...)} with each key being optional
+        Returns a list of Fade objects
         """
-        ranges = {}
-        if self.fade_type in ['LEFT', 'BOTH']:
-            point_start = Vector((sequence.frame_final_start, 0.0))
-            point_end = Vector((sequence.frame_final_start + self.fade_length, self.max_value))
-            ranges['IN'] = (point_start, point_end)
-        if self.fade_type in ['RIGHT', 'BOTH']:
-            point_start = Vector((sequence.frame_final_end - self.fade_length, self.max_value))
-            point_end = Vector((sequence.frame_final_end, 0.0))
-            ranges['OUT'] = (point_start, point_end)
-        return ranges
+        fades = []
+        if self.type in ['IN', 'IN_OUT']:
+            fade = Fade(sequence, 'IN', self.duration_seconds)
+            fades.append(fade)
+        if self.type in ['OUT', 'IN_OUT']:
+            fade = Fade(sequence, 'OUT', self.duration_seconds)
+            fades.append(fade)
+        return fades
+
+    def fade_find_or_create_fcurve(self, context, sequence):
+        """
+        Iterates over all the fcurves until it finds an fcurve with a data path
+        that corresponds to the sequence.
+        Returns the matching FCurve or creates a new one if the function can't find a match.
+        """
+        fade_fcurve = None
+        fcurves = context.scene.animation_data.action.fcurves
+        prop = self.fades[0].animated_property
+        searched_data_path = sequence.path_from_id(prop)
+        for fcurve in fcurves:
+            if fcurve.data_path == searched_data_path:
+                fade_fcurve = fcurve
+                break
+        if not fade_fcurve:
+            fade_fcurve = fcurves.new(data_path=searched_data_path)
+        return fade_fcurve
+
+    def fade_animation_clear(self, context):
+        """
+        Removes existing keyframes in the fades' time range
+        """
+        keyframe_points = self.fade_fcurve.keyframe_points
+        for keyframe in keyframe_points:
+            frame = keyframe.co[0]
+            for fade in self.fades:
+                if fade.start.x < frame < fade.end.x:
+                    keyframe_points.remove(keyframe)
+
+    def fade_animation_create(self):
+        for fade in self.fades:
+            for point in (fade.start, fade.end):
+                self.fade_fcurve.keyframe_points.insert(frame=point.x, value=point.y)
+
+
+class Fade:
+    """
+    Data structure to represent fades
+    """
+    type = ''
+    animated_property = ''
+    duration_frames = -1
+    start, end = Vector((0, 0)), Vector((0, 0))
+
+    def __init__(self, sequence, type, duration_seconds):
+        self.type = type
+        self.animated_property = 'volume' if hasattr(sequence, 'volume') else 'blend_alpha'
+        self.duration_frames = convert_duration_to_frames(bpy.context, duration_seconds)
+
+        max_value = self.calculate_max_value(sequence)
+        if type == 'IN':
+            self.start = Vector((sequence.frame_final_start, 0.0))
+            self.end = Vector((sequence.frame_final_start + self.duration_frames, max_value))
+        elif type == 'OUT':
+            self.start = Vector((sequence.frame_final_end - self.duration_frames, max_value))
+            self.end = Vector((sequence.frame_final_end, 0.0))
 
     def calculate_max_value(self, sequence):
         """
@@ -112,35 +158,5 @@ class POWER_SEQUENCER_OT_fade_add(bpy.types.Operator):
         # TODO: use the highest point in the curve?
         max_value = getattr(sequence, self.animated_property, 1.0)
         max_value = max_value if max_value > 0.0 else 1.0
+        max_value = 1.0
         return max_value
-
-    def fade_find_fcurve(self, context, sequence):
-        """
-        Iterates over all the fcurves until it finds an fcurve with a data path
-        that corresponds to the sequence.
-        Returns the matching FCurve or None if the function can't find a match.
-        """
-        fade_fcurve = None
-        fcurves = context.scene.animation_data.action.fcurves
-        for fcurve in fcurves:
-            if (fcurve.data_path == 'sequence_editor.sequences_all["' +
-                    sequence.name + '"].' + self.animated_property):
-                fade_fcurve = fcurve
-                break
-        return fade_fcurve
-
-    def fade_animation_clear(self, context, sequence):
-        """
-        Removes existing keyframes in the fades' time range
-        """
-        keyframe_points = self.fade_fcurve.keyframe_points
-        for keyframe in keyframe_points:
-            frame = keyframe.co[0]
-            for fade_range in self.fade_ranges.values():
-                if fade_range[0].x <= frame <= fade_range[1].x:
-                    keyframe_points.remove(keyframe)
-
-    def fade_animation_create(self):
-        for fade_range in self.fade_ranges.values():
-            for point in fade_range:
-                self.fade_fcurve.keyframe_points.insert(frame=point.x, value=point.y)
