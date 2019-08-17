cf151b8b7cfb0587114ae0c60189e3575ccfff84
Nathan Lovato <nathan@gdquest.com>
2019-08-17 13:03:57 +0200

    Use Gizmo primary color and increase line thickness in mouse_cut

--- operators/mouse_cut.py
+++ operators/mouse_cut.py
@@ -9,7 +9,7 @@ from .utils.find_strips_mouse import find_strips_mouse
 from .utils.trim_strips import trim_strips
 from .utils.find_snap_candidate import find_snap_candidate
 
-from .utils.draw import draw_line, draw_arrow_head
+from .utils.draw import draw_line, draw_arrow_head, get_color_gizmo_primary
 from .utils.doc import doc_name, doc_idname, doc_brief, doc_description
 
 
@@ -337,22 +337,35 @@ def draw(
     if start.x > end.x:
         start, end = end, start
 
+    color = get_color_gizmo_primary(context)
+
     # Drawing
     bgl.glEnable(bgl.GL_BLEND)
 
-    bgl.glLineWidth(2)
-    draw_line(SHADER, start, end)
-    draw_line(SHADER, Vector((start.x, min_bottom)), Vector((start.x, max_top)))
-    draw_line(SHADER, Vector((end.x, min_bottom)), Vector((end.x, max_top)))
+    bgl.glLineWidth(3)
+    draw_line(SHADER, start, end, color)
+    draw_line(SHADER, Vector((start.x, min_bottom)), Vector(
+        (start.x, max_top)), color)
+    draw_line(SHADER, Vector((end.x, min_bottom)), Vector((end.x, max_top)),
+              color)
 
     if draw_arrows:
-        first_arrow_center = Vector([start.x + ((end.x - start.x) * 0.25), start.y])
-        second_arrow_center = Vector([end.x - ((end.x - start.x) * 0.25), start.y])
+        first_arrow_center = Vector(
+            [start.x + ((end.x - start.x) * 0.25), start.y])
+        second_arrow_center = Vector(
+            [end.x - ((end.x - start.x) * 0.25), start.y])
         arrow_size = Vector([10, 20])
 
-        bgl.glLineWidth(4)
-        draw_arrow_head(SHADER, first_arrow_center, arrow_size)
-        draw_arrow_head(SHADER, second_arrow_center, arrow_size, points_right=False)
+        bgl.glLineWidth(6)
+        draw_arrow_head(SHADER,
+                        first_arrow_center,
+                        arrow_size,
+                        color=color)
+        draw_arrow_head(SHADER,
+                        second_arrow_center,
+                        arrow_size,
+                        points_right=False,
+                        color=color)
 
     bgl.glLineWidth(1)
     bgl.glDisable(bgl.GL_BLEND)
