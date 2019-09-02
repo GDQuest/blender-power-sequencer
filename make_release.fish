d003037f1ed0ef388aa65503969d5d6e0c505f28
Nathan Lovato <nathan@gdquest.com>
2019-09-02 11:50:09 +0200

    Update the release build script

--- make_release.fish
+++ make_release.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/ fish
 set directory "power_sequencer"
 rm -rf $directory
+rm $directory.zip
 mkdir $directory
 cp * -r $directory
-rm -r $directory/**__pycache__ $directory/power-sequencer_updater $directory/GPATH $directory/GRTAGS $directory/GTAGS $directory/*.fish $directory/img/ $directory/$directory
+rm -r $directory/**__pycache__ $directory/power-sequencer_updater $directory/**GPATH $directory/**GRTAGS $directory/**GTAGS $directory/*.fish $directory/img/ $directory/$directory $directory/LICENSE $directory/pyproject.toml $directory/**.md
 zip $directory.zip $directory/**
 rm -rf $directory
