3d2b2a3c13ee28db206c14e9d7ee727735b9a379
Nathan Lovato <nathan@gdquest.com>
2019-09-02 12:17:38 +0200

    Update release packaging script

--- make_release.fish
+++ make_release.fish
@@ -4,6 +4,8 @@ rm -rf $directory
 rm $directory.zip
 mkdir $directory
 cp * -r $directory
-rm -r $directory/**__pycache__ $directory/power-sequencer_updater $directory/**GPATH $directory/**GRTAGS $directory/**GTAGS $directory/*.fish $directory/img/ $directory/$directory $directory/LICENSE $directory/pyproject.toml $directory/**.md
+for i in $directory/**__pycache__ $directory/power-sequencer_updater $directory/**GPATH $directory/**GRTAGS $directory/**GTAGS $directory/*.fish $directory/img/ $directory/$directory $directory/**LICENSE $directory/**MANIFEST.in $directory/**Makefile $directory/**pyproject.toml $directory/**.md $directory/scripts/ShortcutsDocs
+    rm $i -r
+end
 zip $directory.zip $directory/**
 rm -rf $directory
