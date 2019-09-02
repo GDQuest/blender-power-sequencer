257cd235ab742a7d7d375bbbc8c40b94bdfcf7ef
Nathan Lovato <nathan@gdquest.com>
2019-09-02 11:56:29 +0200

    Add GPL 3 license header to every file

--- operators/audiosync/__init__.py
+++ operators/audiosync/__init__.py
@@ -1,1 +1,17 @@
+#
+# Copyright (C) 2016-2019 by Nathan Lovato, Daniel Oakey, Razvan Radulescu, and contributors
+#
+# This file is part of Power Sequencer.
+#
+# Power Sequencer is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Power Sequencer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Power Sequencer. If
+# not, see <https://www.gnu.org/licenses/>.
+#
 from .find_offset import find_offset
