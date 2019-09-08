c2aa33c591afdd2e5ef66ea23a5071dc78b23f95
Nathan Lovato <nathan@gdquest.com>
2019-09-08 12:03:20 +0200

    Update ffmpeg_preset in render preset scripts

--- operators/render_presets/twitter_720p.py
+++ operators/render_presets/twitter_720p.py
@@ -14,34 +14,34 @@
 # You should have received a copy of the GNU General Public License along with Power Sequencer. If
 # not, see <https://www.gnu.org/licenses/>.
 #
-import bpy
+if __name__ == "__main__":
+    import bpy
 
-# Resolution
-bpy.context.scene.render.resolution_x = 1280
-bpy.context.scene.render.resolution_y = 720
-bpy.context.scene.render.resolution_percentage = 100
-bpy.context.scene.render.pixel_aspect_x = 1
-bpy.context.scene.render.pixel_aspect_y = 1
+    render = bpy.context.scene.render
+    render.resolution_x = 1280
+    render.resolution_y = 720
+    render.resolution_percentage = 100
+    render.pixel_aspect_x = 1
+    render.pixel_aspect_y = 1
 
-# FFMPEG
-bpy.context.scene.render.image_settings.file_format = "FFMPEG"
-bpy.context.scene.render.ffmpeg.format = "MPEG4"
-bpy.context.scene.render.ffmpeg.codec = "H264"
+    # FFMPEG
+    render.image_settings.file_format = "FFMPEG"
+    render.ffmpeg.format = "MPEG4"
+    render.ffmpeg.codec = "H264"
 
-bpy.context.scene.render.ffmpeg.constant_rate_factor = "HIGH"
-bpy.context.scene.render.ffmpeg.ffmpeg_preset = "MEDIUM"
+    render.ffmpeg.constant_rate_factor = "HIGH"
+    render.ffmpeg.ffmpeg_preset = "BEST"
 
+    is_ntsc = render.fps != 25
+    if is_ntsc:
+        render.ffmpeg.gopsize = 18
+    else:
+        render.ffmpeg.gopsize = 15
+    render.ffmpeg.use_max_b_frames = False
 
-is_ntsc = bpy.context.scene.render.fps != 25
-if is_ntsc:
-    bpy.context.scene.render.ffmpeg.gopsize = 18
-else:
-    bpy.context.scene.render.ffmpeg.gopsize = 15
-bpy.context.scene.render.ffmpeg.use_max_b_frames = False
-
-bpy.context.scene.render.ffmpeg.video_bitrate = 4000
-bpy.context.scene.render.ffmpeg.maxrate = 4000
-bpy.context.scene.render.ffmpeg.minrate = 0
-bpy.context.scene.render.ffmpeg.buffersize = 224 * 8
-bpy.context.scene.render.ffmpeg.packetsize = 2048
-bpy.context.scene.render.ffmpeg.muxrate = 10080000
+    render.ffmpeg.video_bitrate = 4000
+    render.ffmpeg.maxrate = 4000
+    render.ffmpeg.minrate = 0
+    render.ffmpeg.buffersize = 224 * 8
+    render.ffmpeg.packetsize = 2048
+    render.ffmpeg.muxrate = 10080000
--- operators/render_presets/youtube_1080.py
+++ operators/render_presets/youtube_1080.py
@@ -14,34 +14,33 @@
 # You should have received a copy of the GNU General Public License along with Power Sequencer. If
 # not, see <https://www.gnu.org/licenses/>.
 #
-import bpy
+if __name__ == "__main__":
+    import bpy
 
-# Resolution
-bpy.context.scene.render.resolution_x = 1920
-bpy.context.scene.render.resolution_y = 1080
-bpy.context.scene.render.resolution_percentage = 100
-bpy.context.scene.render.pixel_aspect_x = 1
-bpy.context.scene.render.pixel_aspect_y = 1
+    render = bpy.context.scene.render
+    render.resolution_x = 1920
+    render.resolution_y = 1080
+    render.resolution_percentage = 100
+    render.pixel_aspect_x = 1
+    render.pixel_aspect_y = 1
 
-# FFMPEG
-bpy.context.scene.render.image_settings.file_format = "FFMPEG"
-bpy.context.scene.render.ffmpeg.format = "MPEG4"
-bpy.context.scene.render.ffmpeg.codec = "H264"
+    render.image_settings.file_format = "FFMPEG"
+    render.ffmpeg.format = "MPEG4"
+    render.ffmpeg.codec = "H264"
 
-bpy.context.scene.render.ffmpeg.constant_rate_factor = "PERC_LOSSLESS"
-bpy.context.scene.render.ffmpeg.ffmpeg_preset = "GOOD"
+    render.ffmpeg.constant_rate_factor = "PERC_LOSSLESS"
+    render.ffmpeg.ffmpeg_preset = "BEST"
 
+    is_ntsc = render.fps != 25
+    if is_ntsc:
+        render.ffmpeg.gopsize = 18
+    else:
+        render.ffmpeg.gopsize = 15
+    render.ffmpeg.use_max_b_frames = False
 
-is_ntsc = bpy.context.scene.render.fps != 25
-if is_ntsc:
-    bpy.context.scene.render.ffmpeg.gopsize = 18
-else:
-    bpy.context.scene.render.ffmpeg.gopsize = 15
-bpy.context.scene.render.ffmpeg.use_max_b_frames = False
-
-bpy.context.scene.render.ffmpeg.video_bitrate = 9000
-bpy.context.scene.render.ffmpeg.maxrate = 9000
-bpy.context.scene.render.ffmpeg.minrate = 0
-bpy.context.scene.render.ffmpeg.buffersize = 224 * 8
-bpy.context.scene.render.ffmpeg.packetsize = 2048
-bpy.context.scene.render.ffmpeg.muxrate = 10080000
+    render.ffmpeg.video_bitrate = 9000
+    render.ffmpeg.maxrate = 9000
+    render.ffmpeg.minrate = 0
+    render.ffmpeg.buffersize = 224 * 8
+    render.ffmpeg.packetsize = 2048
+    render.ffmpeg.muxrate = 10080000
