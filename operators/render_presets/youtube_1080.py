9ba57426cec32e447b07b06569308f23ac348b87
Nathan Lovato <nathan@gdquest.com>
2019-09-30 16:28:35 +0200

    Fix youtube rendering preset

--- operators/render_presets/youtube_1080.py
+++ operators/render_presets/youtube_1080.py
@@ -4,7 +4,20 @@
 # This file is part of Power Sequencer.
 #
 # Power Sequencer is free software: you can redistribute it and/or modify it under the terms of the
-# GNU General Public License as.render
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Power Sequencer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Power Sequencer. If
+# not, see <https://www.gnu.org/licenses/>.
+#
+if __name__ == "__main__":
+    import bpy
+
+    render = bpy.context.scene.render
     render.resolution_x = 1920
     render.resolution_y = 1080
     render.resolution_percentage = 100
@@ -18,6 +31,7 @@
     render.ffmpeg.constant_rate_factor = "PERC_LOSSLESS"
     render.ffmpeg.ffmpeg_preset = "BEST"
 
+    scene = bpy.context.scene
     fps = scene.render.fps / scene.render.fps_base
     render.ffmpeg.gopsize = round(fps / 2.0)
     render.ffmpeg.use_max_b_frames = True
@@ -30,5 +44,5 @@
     render.ffmpeg.packetsize = 2048
     render.ffmpeg.muxrate = 10080000
 
-    ffmpeg.audio_codec = "AAC"
-    ffmpeg.audio_bitrate = 384
+    render.ffmpeg.audio_codec = "AAC"
+    render.ffmpeg.audio_bitrate = 384
