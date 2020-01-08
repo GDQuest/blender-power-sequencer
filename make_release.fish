bf8136bf0683b1215ac52a1545c255a14ffa9f19
Nathan Lovato <nathan@gdquest.com>
2020-01-08 22:41:01 +0100

    Update make release script

--- make_release.fish
+++ make_release.fish
@@ -1,11 +1,26 @@
-#!/usr/bin/ fish
-set directory "power_sequencer"
-rm -rf $directory
-rm $directory.zip
-mkdir $directory
-cp * -r $directory
-for i in $directory/**__pycache__ $directory/power-sequencer_updater $directory/**GPATH $directory/**GRTAGS $directory/**GTAGS $directory/*.fish $directory/img/ $directory/$directory $directory/**LICENSE $directory/**MANIFEST.in $directory/**Makefile $directory/**pyproject.toml $directory/**.md $directory/scripts/ShortcutsDocs
-    rm $i -r
+#!/usr/bin/env fish
+
+function make_release
+    set out_path "power_sequencer"
+
+    echo "Deleting the previous release archive."
+    rm $out_path.zip
+
+    echo "Copying the add-on files and development files to the" $out_path "directory".
+    mkdir $out_path && cp -r (string match -v $out_path -- *) $out_path
+
+    echo "Deleting development files from the release copy."
+    cd $out_path
+    # Enumerate files for wildcards without matches
+    for i in **__pycache__ **TAGS **GPATH **GRTAGS **GTAGS *.fish img/ **LICENSE **MANIFEST.in **Makefile **pyproject.toml **.md scripts/ShortcutsDocs
+        rm -r $i
+    end
+    cd ..
+
+    echo "Compressing the release directory to a zip archive."
+    zip $out_path.zip $out_path/**
+    echo "Deleting the release copy directory."
+    rm -rf $out_path
 end
-zip $directory.zip $directory/**
-rm -rf $directory
+
+make_release
