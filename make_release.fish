9c593a4ec73d6a15c275e62c8aa6b68bf9fe03c1
Nathan Lovato <nathan@gdquest.com>
2020-03-19 18:50:40 -0600

    Update make_release script

--- make_release.fish
+++ make_release.fish
@@ -1,26 +1,32 @@
 #!/usr/bin/env fish
 
+set blender_addons_repo $HOME/Repositories/applications/blender-git/blender/release/scripts/addons/
+
 function make_release
     set out_path "power_sequencer"
 
-    echo "Deleting the previous release archive."
-    rm $out_path.zip
-
-    echo "Copying the add-on files and development files to the" $out_path "directory".
-    mkdir $out_path && cp -r (string match -v $out_path -- *) $out_path
+    rm -v $out_path.zip
+    mkdir -v $out_path && cp -rv (string match -v $out_path -- *) $out_path
 
     echo "Deleting development files from the release copy."
     cd $out_path
     # Enumerate files for wildcards without matches
     for i in **__pycache__ **TAGS **GPATH **GRTAGS **GTAGS *.fish img/ **LICENSE **MANIFEST.in **Makefile **pyproject.toml **.md scripts/ShortcutsDocs
-        rm -r $i
+        rm -rv $i
     end
     cd ..
 
     echo "Compressing the release directory to a zip archive."
     zip $out_path.zip $out_path/**
+
+    echo Copying the files to the blender addons repository
+    rm -r $blender_addons_repo/$out_path
+    cp -r $out_path $blender_addons_repo
+
     echo "Deleting the release copy directory."
-    rm -rf $out_path
+    rm -rfv $out_path
+
+    echo "Done. Build with Blender, run `make test`, and test in a development build before pushing."
 end
 
 make_release
