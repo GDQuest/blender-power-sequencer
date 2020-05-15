060c59bedce8458b4450fd9f449ec0574d813d3c
Nathan Lovato <nathan@gdquest.com>
2020-05-14 19:12:18 -0600

    Update the release build script
    Produces builds for both GitHub and Blender official

--- make_release.fish
+++ make_release.fish
@@ -1,32 +1,38 @@
 #!/usr/bin/env fish
 
 set blender_addons_repo $HOME/Repositories/applications/blender-git/blender/release/scripts/addons/
+set out_path power_sequencer
 
 function make_release
-    set out_path "power_sequencer"
-
     rm -v $out_path.zip
-    mkdir -v $out_path && cp -rv (string match -v $out_path -- *) $out_path
+    mkdir -v $out_path
+    echo "Copying all files and directories to $out_path."
+    cp -r (string match -v $out_path -- *) $out_path
 
     echo "Deleting development files from the release copy."
     cd $out_path
     # Enumerate files for wildcards without matches
-    for i in **__pycache__ **TAGS **GPATH **GRTAGS **GTAGS *.fish img/ **LICENSE **MANIFEST.in **Makefile **pyproject.toml **.md scripts/ShortcutsDocs
-        rm -rv $i
+    for i in **__pycache__ *.fish img/ **LICENSE **MANIFEST.in **Makefile **pyproject.toml **.md scripts/ShortcutsDocs
+        rm -r $i
     end
     cd ..
 
-    echo "Compressing the release directory to a zip archive."
-    zip $out_path.zip $out_path/**
+    echo "Compressing the release directory to a zip archive for the GitHub release."
+    zip --quiet $out_path.zip $out_path/**
 
-    echo Copying the files to the blender addons repository
+    echo "Making the blender version."
+    sed -i s/blender_power_sequencer/power_sequencer/ $out_path/tools/__init__.py $out_path/operators/__init__.py
+
+    echo "Copying the files to the Blender Addons repository."
+    echo "Removing $blender_addons_repo/$out_path."
     rm -r $blender_addons_repo/$out_path
+    echo "Copying $out_path to $blender_addons_repo."
     cp -r $out_path $blender_addons_repo
 
-    echo "Deleting the release copy directory."
-    rm -rfv $out_path
+    echo "Deleting the build directory $out_path."
+    rm -rf $out_path
 
-    echo "Done. Build with Blender, run `make test`, and test in a development build before pushing."
+    echo "Done. Build with Blender, run `make update && make test`, and test in a development build before pushing."
 end
 
 make_release
