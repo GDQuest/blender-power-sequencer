dbfafacbfadfab86f080dd2336edfc41ae2fd7bd
GTaulats <74924552+GTaulats@users.noreply.github.com>
2023-02-28 20:42:07 +0100

    Remove unused variables functions.py (#470)

--- operators/utils/functions.py
+++ operators/utils/functions.py
@@ -7,9 +7,6 @@ import bpy
 
 from .global_settings import SequenceTypes
 
-max_channel = 32
-min_channel = 1
-
 
 def calculate_distance(x1, y1, x2, y2):
     return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
